3cf51cccd9211298d95bc725b30ec33f2f031b5e
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-10-05 07:31:38 -0700

    feat: view-and-download-results-desktop

--- modules/assessment/index.tsx
+++ modules/assessment/index.tsx
@@ -1,1 +1,19 @@
-// Silence is golden
+import React from 'react';
+import FeedbackHeader from './component/FeedbackHeader';
+// import Feedcard from './component/Feedcard'
+
+const beginer = [
+  {
+    score: '90%',
+  },
+];
+const Assesment = () => {
+  return (
+    <div className=" bg-white-300">
+      <FeedbackHeader />
+      {/* <Feedcard /> */}
+    </div>
+  );
+};
+
+export default Assesment;
---
+++ pages/assesment.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Assesment from '../modules/assessment';
+
+const assesment = () => {
+  return (
+    <>
+      <Assesment />
+    </>
+  );
+};
+
+export default assesment;
