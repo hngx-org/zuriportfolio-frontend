fee33eb35ad91d7a3ec4acc365ef18d2b5c51eb5
homoakin619 <homohaykins619@gmail.com>
2023-10-05 22:13:12 +0100

    removed edit on tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,6 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
     extend: {
       colors: {
         dark: {
