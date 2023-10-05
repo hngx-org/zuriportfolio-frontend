af0dd90fc39e9938f4d8b93e9d00943e99d152d7
Emujede Hilary <104493382+dev-hills@users.noreply.github.com>
2023-10-05 21:57:52 +0100

    Created Product Card Component for Zuri Marketplace

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -14,3 +14,24 @@ export interface MainLayoutContextProps {
   activePage?: string;
   setActivePage: (page: string) => void;
 }
+
+export interface ProductCardProps {
+  image: string;
+  productName: string;
+  productPrice: string;
+  productOwner: string;
+  productRating: number;
+  showLimitedOffer?: boolean;
+  showTopPicks?: boolean;
+  showDiscount?: boolean;
+  discount?: number;
+}
+
+export interface ratingProps {
+  src: string;
+  alt: string;
+}
+
+export interface starProps {
+  [key: number]: ratingProps;
+}
