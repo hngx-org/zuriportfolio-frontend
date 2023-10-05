8a1843be2f53825b9c8482e374fe61f644ce48c9
STEPHEN ONOKWAKPOR <oghenekparobo>
2023-10-05 17:04:48 +0100

    feat: added the cart summary modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import MainLayout from '../components/Layout/MainLayout';
+import Summary from '../components/cart/checkout/Summary';
 
 function Home() {
   return (
     <MainLayout activePage="home" showDashboardSidebar showTopbar>
-      <div className="w-full flex items-start justify-start"></div>
+      <div className="w-full flex items-start justify-start">
+        <Summary />
+      </div>
     </MainLayout>
   );
 }
