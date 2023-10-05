6c8dce1a8a1f9d5d36882203190a7036d7d40275
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-10-05 10:04:25 -0700

    update the assesment badge

--- modules/assessment/index.tsx
+++ modules/assessment/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import FeedbackHeader from './component/FeedbackHeader';
-// import Feedcard from './component/Feedcard'
+import Feedcard from './component/Feedcard';
+import expertBag from '../../public/assets/images/banner/expertBag.png';
+import beginner from '../../public/assets/images/banner/bignneerbadge.png';
+import intermidiate from '../../public/assets/images/banner/intermediateBadge.png';
 
-const beginer = [
-  {
-    score: '90%',
-  },
-];
 const Assesment = () => {
+  const exepert = {
+    score: '90%',
+    img: expertBag,
+    badgeName: 'expert',
+  };
+  const midiate = {
+    score: '79%',
+    img: intermidiate,
+    retake: 'Retake',
+    badgeName: 'intermediate',
+  };
+  const beginer = {
+    score: '49%',
+    img: expertBag,
+    retake: 'Retake',
+    intermediate: 'beginner',
+  };
   return (
     <div className=" bg-white-300">
       <FeedbackHeader />
-      {/* <Feedcard /> */}
+      <Feedcard score={exepert.score} badge={exepert.img} badgeName={exepert.badgeName} retake="" />
     </div>
   );
 };
