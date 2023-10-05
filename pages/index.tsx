d623fd43ecdd5c121620c7405f33c43cd2ce71c6
Samson Ocran <samson.ocran@gmail.com>
2023-10-06 00:02:36 +0100

    paymentsuccessful modal implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import MainLayout from '../components/Layout/MainLayout';
 import ProductDetailsDescription from '../modules/marketplace/productDetailsDescription';
+import CartPaymentModal from '../components/Modals/CartPaymentModal';
 
 function Home() {
   return (
     <MainLayout activePage="home" showDashboardSidebar showTopbar>
       <div className="w-full flex items-start justify-start"></div>
+      <CartPaymentModal />
     </MainLayout>
   );
 }
