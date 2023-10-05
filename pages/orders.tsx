1b7fabf5f880089fd5888f262e73e848b309f052
gideon-del <gideonchidi471@gmail.com>
2023-10-05 18:15:15 +0100

    fix: pulling from dev

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavDashBoard from '../modules/dashboard/component/Navbar';
+import { SearchNormal1 } from 'iconsax-react';
 
 const Orders: React.FC = () => {
   return (
@@ -18,9 +19,17 @@ const Orders: React.FC = () => {
               View Order Details
             </button>
           </nav>
-          <div>
-            <div></div>
-          </div>
+          <section className="rounded">
+            <div>
+              <div className="focus-within:outline focus-within:outline-black px-[14px] py-[10px] flex gap-2 items-center ">
+                <SearchNormal1 size="4" color="#667085" />
+                <input
+                  className=" bg-transparent focus-within:outline-none flex-1 text-[1rem] leading-[150%]"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+          </section>
         </section>
       </main>
     </div>
