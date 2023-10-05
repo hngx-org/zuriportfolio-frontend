57e0e3023c63ac2ae2a2d48de03ea1d1be74c3ec
olafisoyevince <olafisoyevincent@gmail.com>
2023-10-06 00:20:20 +0100

    refactor: changed the img tags to next Image tags

--- modules/auth/component/verificationLayout.tsx
+++ modules/auth/component/verificationLayout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import MainLayout from '../../../components/Layout/MainLayout';
 import { VerificationLayoutProps } from '../../../@types';
 
@@ -6,16 +7,20 @@ function VerificationLayout({ children }: VerificationLayoutProps) {
   return (
     <MainLayout showDashboardSidebar={false} showTopbar={false} showFooter={false} activePage="verification">
       <div className=" h-[100dvh] w-full relative overflow-hidden flex items-center justify-center">
-        <img
-          className=" absolute top-0 right-0 hidden md:block md:w-[200px] md:h-[200px]"
+        <Image
+          className="absolute top-0 right-0 hidden md:block md:w-[200px] md:h-[200px]"
           src="/assets/images/blob-os.svg"
-          alt=""
+          alt="blob"
+          width={200}
+          height={200}
         />
         {children}
-        <img
+        <Image
           className="absolute bottom-0 left-0 w-[130px] h-[130px] md:w-[180px] md:h-[180px] lg:w-[200px] lg:h-[200px]"
           src="/assets/images/unlock-os.svg"
-          alt=""
+          alt="an unlocked padlock"
+          width={200}
+          height={200}
         />
       </div>
     </MainLayout>
