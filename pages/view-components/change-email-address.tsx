3c045b4e9fe23388e3e83ffa049fb00203742e5d
olafisoyevince <olafisoyevincent@gmail.com>
2023-10-06 10:58:59 +0100

    feat: created the change email address page

---
+++ pages/view-components/change-email-address.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChangeEmailAddress from '../../modules/auth/changeEmailAddress';
+import VerificationLinkSent from '../../modules/auth/verificationLinkSent';
+
+const ChangeEmailAddressPage = () => {
+  return (
+    // <VerificationLinkSent />
+    <ChangeEmailAddress />
+  );
+};
+
+export default ChangeEmailAddressPage;
