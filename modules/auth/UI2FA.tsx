390d2ba1b4eddc6a9670fc7763415a05e715422b
Heba-Webdev23 <jtvonline@yahoo.com>
2023-10-06 18:31:33 +0300

    Fixed position of absolute images

--- modules/auth/UI2FA.tsx
+++ modules/auth/UI2FA.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 function UI2FA() {
     return (
         <>
-              <header className="w-full py-4
+              <header className="py-4 md:py-8
       border border-gray-300 border-opacity-40 border-x-0
-      border-t-0 px-4 md:hidden relative z-3">
+      border-t-0 px-4 lg:hidden z-2 bg-white-100">
         <Link href={'/'}>
           <Image
           src="/assets/auth/zuri_logo.svg"
@@ -24,7 +25,7 @@ function UI2FA() {
         width={780}
         height={500}
         style={{ width: "950px", height: "100%" }}
-        className="hidden md:block"
+        className="hidden lg:block"
       />
       </div>
         <Image
@@ -32,17 +33,17 @@ function UI2FA() {
           alt=""
           width={120}
           height={100}
+          priority
           style={{ width: "280px", height: "100%" }}
-          className="hidden md:block lg:hidden absolute bottom-[-0%] left-[-21.5%] z-[-1] opacity-70"
+          className="absolute hidden md:block lg:hidden  bottom-[-42%] left-[-15.5%] opacity-70"
         />
         <Image
-          src="/assets/auth/shape.svg"
+          src="/assets/auth/auth-shape.svg"
           alt=""
           width={120}
           height={100}
-          style={{ width: "280px", height: "100%" }}
-          className="absolute bottom-[88%] md:bottom-[93%] left-[80%]
-          lg:left-[70%] z-[-1] opacity-40 hidden md:block"
+          style={{ width: "180px", height: "150px" }}
+          className="absolute top-[10%] lg:top-[0%] right-0 z-[-1] opacity-40 hidden md:block"
         />
         </>
     )
