5c15295778bae3af92d9b14028cbbf9d14c76551
Meekaylah <kaylahusanga@gmail.com>
2023-10-06 15:21:01 +0100

    style: Aligned code using yarn format

--- modules/shop/productDetails.tsx
+++ modules/shop/productDetails.tsx
@@ -8,58 +8,61 @@ import Button from '@ui/Button';
 import { ProfileCircle } from 'iconsax-react';
 import { ArrowLeft } from 'iconsax-react';
 
-
 const lens = document.getElementById('lens');
 const second = document.getElementById('second');
 
 function zoomOnHover(e: React.MouseEvent<HTMLDivElement>) {
-    const target = e.target as HTMLDivElement;
-    const offsetLeft = target.offsetLeft;
-    const offsetTop = target.offsetTop;
-  
-    var x = e.clientX - offsetLeft
-    var y = e.clientY - offsetTop
+  const target = e.target as HTMLDivElement;
+  const offsetLeft = target.offsetLeft;
+  const offsetTop = target.offsetTop;
 
-    if (lens && lens.style) {
-        lens.style.display = 'block';
-        lens.style.left = x + 'px';
-        lens.style.top = y + 40 + 'px';
-      }
+  var x = e.clientX - offsetLeft;
+  var y = e.clientY - offsetTop;
 
-    if (second && second.style) {
-        second.style.backgroundPosition = (x - (500 / 2 / 6)) * -6 + 'px ' + (y - (500 / 2 / 6) + 250) * -6 + 'px';
-    }
+  if (lens && lens.style) {
+    lens.style.display = 'block';
+    lens.style.left = x + 'px';
+    lens.style.top = y + 40 + 'px';
   }
 
+  if (second && second.style) {
+    second.style.backgroundPosition = (x - 500 / 2 / 6) * -6 + 'px ' + (y - 500 / 2 / 6 + 250) * -6 + 'px';
+  }
+}
+
 function mouseOut() {
-    if(lens && lens.style){
-        lens.style.display = 'none';
-    }
+  if (lens && lens.style) {
+    lens.style.display = 'none';
+  }
 }
 
 export default function ProductDetails() {
   const router = useRouter();
 
   return (
-    
     <main className={`flex flex-col items-center lg:px-12 md:px-10 px-6 lg:pt-6 pt-4 lg:pb-6 pb-4`}>
-    <ArrowLeft size="32" color="#000000" className='self-start'/>
+      <ArrowLeft size="32" color="#000000" className="self-start" />
       {/* Product Details  */}
       <div className="flex lg:flex-row flex-col items-center justify-center gap-x-6 w-full">
         {/* Product Detail Images  */}
         <div className="flex flex-col w-full item-center lg:gap-y-4">
-            <div>
-                <div id='first' onMouseOut={mouseOut} onMouseMove={e => zoomOnHover(e)} className='peer'>
-                <span className='w-32 h-32 bg-black/40 absolute border-2 translate-x-2/4 -translate-y-2/4 hidden' id='lens'></span>
-                <Image
+          <div>
+            <div id="first" onMouseOut={mouseOut} onMouseMove={(e) => zoomOnHover(e)} className="peer">
+              <span
+                className="w-32 h-32 bg-black/40 absolute border-2 translate-x-2/4 -translate-y-2/4 hidden"
+                id="lens"
+              ></span>
+              <Image
                 src={mainImage}
                 alt="Main Image"
                 className="lg:w-full md:w-[750px] w-[350px] lg:h-[520px] md:h-[600px] h-[340px] lg:object-cover object-contain rounded-3xl"
-                />
-                </div>
-                <div className='absolute w-2/4 h-[65vh] left-[50%] top-8 overflow-hidden bg-black bg-[size:600%_600%] bg-[url("../public/assets/mainImage.png")] rounded-3xl hidden peer-hover:block' id='second'>
-                </div>
+              />
             </div>
+            <div
+              className='absolute w-2/4 h-[65vh] left-[50%] top-8 overflow-hidden bg-black bg-[size:600%_600%] bg-[url("../public/assets/mainImage.png")] rounded-3xl hidden peer-hover:block'
+              id="second"
+            ></div>
+          </div>
           <Slider />
         </div>
 
