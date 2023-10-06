4bd4d798a98d9b9f36b773d7948772fb2ee9af56
Shalom Taiwo <ifeshalomt@gmail.com>
2023-10-06 19:19:06 +0200

    comment out the withAuth and withOutAuth

--- helpers/withAuth.tsx
+++ helpers/withAuth.tsx
@@ -1,26 +1,26 @@
-import { useEffect } from 'react';
-import Router, { useRouter } from 'next/router';
-import isAuthenticated from './isAuthenticated';
+// import { useEffect } from 'react';
+// import Router, { useRouter } from 'next/router';
+// import isAuthenticated from './isAuthenticated';
 
-// this would change later on once backend has the authentication
-// working.
-const withAuth = <P extends {}>(WrappedComponent: React.ComponentType<P>) => {
-  const Wrapper: React.FC<P> = (props) => {
-    const router = useRouter();
+// // this would change later on once backend has the authentication
+// // working.
+// const withAuth = <P extends {}>(WrappedComponent: React.ComponentType<P>) => {
+//   const Wrapper: React.FC<P> = (props) => {
+//     const router = useRouter();
 
-    useEffect(() => {
-      const token = localStorage.getItem('authToken');
-      const isLoggedIn = isAuthenticated(token as string);
-      if (!isLoggedIn) {
-        router.push('/auth/login');
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+//     useEffect(() => {
+//       const token = localStorage.getItem('authToken');
+//       const isLoggedIn = isAuthenticated(token as string);
+//       if (!isLoggedIn) {
+//         router.push('/auth/login');
+//       }
+//       // eslint-disable-next-line react-hooks/exhaustive-deps
+//     }, []);
 
-    return <WrappedComponent {...props} />;
-  };
+//     return <WrappedComponent {...props} />;
+//   };
 
-  return Wrapper;
-};
+//   return Wrapper;
+// };
 
-export default withAuth;
+// export default withAuth;
