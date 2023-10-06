3545f8c2ce30796a35a22d90393347fe11f26f22
olafisoyevince <olafisoyevincent@gmail.com>
2023-10-06 16:07:08 +0100

    feat: created the verification complete page

--- pages/auth/verification.tsx
+++ pages/auth/verification.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import VerificationLinkSent from '../../modules/auth/verificationLinkSent';
 import VerificationLayout from '../../modules/auth/component/verificationLayout';
+import VerificationComplete from '../../modules/auth/verificationComplete';
+import ChangeEmailAddress from '../../modules/auth/changeEmailAddress';
 
 function Verification() {
-  return <VerificationLinkSent />;
+  return (
+    <>
+      {/* <VerificationLinkSent /> */}
+      {/* <ChangeEmailAddress /> */}
+      <VerificationComplete />
+    </>
+  );
 }
 
 export default Verification;
