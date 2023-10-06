e9f73b64a07202086a09145dc816e5934dc16907
BaZz <bazuaye50@gmail.com>
2023-10-05 20:30:23 -0700

    refactor: updated Logo used from PNG to SVG

--- components/Navbars/TopBar.tsx
+++ components/Navbars/TopBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from './assets/zuriLogo.png';
+import logo from './assets/zuriLogo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
