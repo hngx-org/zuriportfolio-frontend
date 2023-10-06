577a9e370778f6bedd8a5e55281d68b97961aba5
ayo <ayomikuntemitope246@gmail.com>
2023-10-06 10:36:27 +0100

    complete RemoveCart Modal feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import React, { useState } from 'react'; // Import React and useState
 import MainLayout from '../components/Layout/MainLayout';
 import CategoriesNav from '../modules/marketplace/component/CategoriesNav/CategoriesNav';
 import ProductDetailsDescription from '../modules/marketplace/productDetailsDescription';
+import RemoveCart from '../components/Modals/Removecart'; // Import the RemoveCart component
 
 function Home() {
   const navItems: string[] = [
@@ -15,9 +17,29 @@ function Home() {
     ' More...',
   ];
 
+  // State to control the visibility of the RemoveCart modal
+  const [isRemoveCartModalOpen, setIsRemoveCartModalOpen] = useState(false);
+
+  // Function to open the RemoveCart modal
+  const openRemoveCartModal = () => {
+    setIsRemoveCartModalOpen(true);
+  };
+
+  // Function to close the RemoveCart modal
+  const closeRemoveCartModal = () => {
+    setIsRemoveCartModalOpen(false);
+  };
+
   return (
     <MainLayout activePage="home" showDashboardSidebar showTopbar>
       <CategoriesNav navItems={navItems} />
+
+      {/* Your existing content */}
+
+      <h1>Welcome to the Home Page</h1>
+      <button onClick={openRemoveCartModal}>Remove Cart</button>
+      {/* Render the RemoveCart modal conditionally based on state */}
+      {isRemoveCartModalOpen && <RemoveCart onClose={closeRemoveCartModal} />}
     </MainLayout>
   );
 }
