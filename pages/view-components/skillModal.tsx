da20369c9d00cdf07c1cb858257c2b9ad5798576
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-10-07 20:16:17 +0100

    Style: icons updates

---
+++ pages/view-components/skillModal.tsx
@@ -0,0 +1,10 @@
+import SkillModal from '@modules/portfolio/component/skillModal/SkillsModal'
+import React from 'react'
+
+const skillModal = () => {
+  return (
+    <SkillModal/>
+  )
+}
+
+export default skillModal
