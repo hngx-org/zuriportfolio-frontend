79784525ee87c063a97f7d6bc61594eb32e9602b
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-10-07 16:11:40 +0100

    message

--- modules/marketplace/component/landingpage/productCardWrapper/product-card-wrapper.tsx
+++ modules/marketplace/component/landingpage/productCardWrapper/product-card-wrapper.tsx
@@ -3,26 +3,19 @@ import ProductCard from '../../ProductCard';
 import { ProductCardProps } from '../../../../../@types';
 import Link from 'next/link';
 import styles from './product-card-wrapper.module.css';
-import { useRouter } from 'next/router';
 
 function ProductCardWrapper({ productsList, title }: { productsList: ProductCardProps[]; title: string }) {
-  const router = useRouter();
-  const isLgHidden = router.pathname !== '/marketplace';
   return (
     <section className="w-full mb-2.5 md:mb-8 pt-2.5">
-      <h3 className="text-[#101928] font-manropeL mb-5 md:mb-8 font-bold md:text-2xl leading-normal flex items-center justify-between">
+      <h3 className="text-custom-color31 font-manropeL mb-5 md:mb-8 font-bold md:text-2xl leading-normal flex items-center justify-between">
         {title}
-        <Link
-          className={`flex items-center gap-2 text-sm font-bold text-brand-green-shade50 ${
-            isLgHidden ? '' : 'lg:hidden'
-          }`}
-          href=""
-        >
+
+        <Link className="flex lg:hidden items-center gap-2 text-sm font-bold text-brand-green-shade50" href="">
           View All
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
             <path
               d="M7.42578 16.5999L12.8591 11.1666C13.5008 10.5249 13.5008 9.4749 12.8591 8.83324L7.42578 3.3999"
-              stroke="#00894C"
+              className=" stroke-green-300"
               strokeWidth="1.5"
               strokeMiterlimit="10"
               strokeLinecap="round"
