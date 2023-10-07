259b6f9c6d041912956a7247a99595ea08f08056
SetUp <abdulmumeenibroheem51@gmail.com>
2023-10-07 23:44:24 +0100

    created data arrays

---
+++ modules/assessment/component/questionsArr.tsx
@@ -0,0 +1,87 @@
+export const questionArr = [
+  {
+    id: 1,
+    question: 'What is the primary goal of a &apos;landing page in digital marketing?',
+    tip: 'Pick only one correct answer',
+    options: [
+      'To provide customer support',
+      'To showcase the company&apos;s history',
+      'To collect visitor information or encourage a specific action',
+      'To display a blog',
+    ],
+  },
+  {
+    id: 2,
+    question: 'In digital marketing, a higher bounce rate on a website is always a negative indicator of performance.',
+    tip: 'Choose either true of false',
+    options: ['True', 'False'],
+  },
+  {
+    id: 3,
+    question: 'What is the primary goal of a &apos;landing page&apos; in digital marketing?',
+    tip: 'Pick only one correct answer',
+    options: [
+      'To provide customer support',
+      'To showcase the company&apos;s history',
+      'To collect visitor information or encourage a specific action',
+      'To display a blog',
+    ],
+  },
+  {
+    id: 4,
+    question: 'In digital marketing, a higher bounce rate on a website is always a negative indicator of performance.',
+    tip: 'Choose either true of false',
+    options: ['True', 'False'],
+  },
+  {
+    id: 5,
+    question: 'What is the primary goal of a &apos;landing page&apos; in digital marketing?',
+    tip: 'Pick only one correct answer',
+    options: [
+      'To provide customer support',
+      'To showcase the company&apos;s history',
+      'To collect visitor information or encourage a specific action',
+      'To display a blog',
+    ],
+  },
+  {
+    id: 6,
+    question: 'In digital marketing, a higher bounce rate on a website is always a negative indicator of performance.',
+    tip: 'Choose either true of false',
+    options: ['True', 'False'],
+  },
+  {
+    id: 7,
+    question: 'What is the primary goal of a &apos;landing page&apos; in digital marketing?',
+    tip: 'Pick only one correct answer',
+    options: [
+      'To provide customer support',
+      'To showcase the company&apos;s history',
+      'To collect visitor information or encourage a specific action',
+      'To display a blog',
+    ],
+  },
+  {
+    id: 8,
+    question: 'In digital marketing, a higher bounce rate on a website is always a negative indicator of performance.',
+    tip: 'Choose either true of false',
+    options: ['True', 'False'],
+  },
+  {
+    id: 9,
+    question: 'What is the primary goal of a &apos;landing page&apos; in digital marketing?',
+    tip: 'Pick only one correct answer',
+    options: [
+      'To provide customer support',
+      'To showcase the company&apos;s history',
+      'To collect visitor information or encourage a specific action',
+      'To display a blog',
+    ],
+  },
+  {
+    id: 10,
+    question: 'In digital marketing, a higher bounce rate on a website is always a negative indicator of performance.',
+    tip: 'Choose either true of false',
+    options: ['True', 'False'],
+  },
+];
