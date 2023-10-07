dfdef727eb64b8b544148420f4eb5d55d0e2498b
Olaoti <omotolaolaoti@gmail.com>
2023-10-07 10:44:09 +0100

    CRE-1 done

--- modules/assessment/component/Description.tsx
+++ modules/assessment/component/Description.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 
 function Description({ info, number, icon }: any) {
   return (
-    <div className="description flex border-[1px] border-[#A8ACAB] p-6 rounded-lg hover:text-[#005427] font-ManropeL items-center justify-between w-[291px]">
+    <div className="description flex border-[1px] border-[#A8ACAB] p-4 md:p-6 rounded-lg hover:text-[#005427] font-ManropeL items-center justify-between w-[291px]">
       <div>
         <div className="text-base uppercase">{info}</div>
-        <div className="text-[#006F37]  text-[45px] leading-[53px] mt-2">{number}</div>
+        <div className="text-[#006F37] text-[30px]  md:text-[45px] md:leading-[53px] mt-2">{number}</div>
       </div>
       <div className="w-[48px] h-[48px] bg-[#CAEAD4] grid place-items-center rounded-full">
         <Image src={icon} width="27.61" height="27.61" alt="Icon" />
--- pages/assessment/index.tsx
+++ pages/assessment/index.tsx
@@ -35,27 +35,29 @@ function Index() {
   return (
     <div className="w-full items-start justify-start">
       <Header heading={'Admin Assessment Board'} body={'For the general creation and management of assessments'} />
-      <div className="assessment-body text-Manrope mx-[24px] lg:mx-[104px]">
-        <div className="assessment-links mt-[95px] flex px-[100px] gap-[13vw]">
-          <div className="flex-1 border-[1px] border-[#A8ACAB] p-6 rounded-lg hover:text-[#005427]">
+      <div className="assessment-body text-Manrope mx-[16px] lg:mx-[104px]">
+        <div className="assessment-links mt-[50px] md:mt-[95px] flex px:[12px] md:px-[50px] lg:px-[100px] gap-4 md:gap-[13vw]">
+          <div className="flex-1 border-[1px] border-[#A8ACAB] p-2 gap:p-6 rounded-lg hover:text-[#005427]">
             <Link className="w-full grid place-items-center" href="/assessment/new">
               <Image src={addmessage} width="33" height="33" alt="add message" />
-              <div className="text-base font-semibold font-ManropeB pt-[9px]">Create new assesment</div>
+              <div className="text-sm md:text-base font-semibold font-ManropeB pt-[9px]">Create new assesment</div>
             </Link>
           </div>
-          <div className="flex-1 center border-[1px] border-[#A8ACAB] p-6 rounded-lg hover:text-[#005427]">
+          <div className="flex-1 center border-[1px] border-[#A8ACAB] p-2 gap:p-6 rounded-lg hover:text-[#005427]">
             <Link className="grid place-items-center w-full" href="/assessment/drafts">
               <Image src={draftsimg} width="33" height="33" alt="go to drafts" />
-              <div className="grid place-items-center text-base font-semibold font-ManropeB pt-[9px]">My drafts</div>
+              <div className="grid place-items-center text-sm md:text-base font-semibold font-ManropeB pt-[9px]">
+                My drafts
+              </div>
             </Link>
           </div>
         </div>
-        <div className="board-desc flex gap-[26px] justify-center w-full my-[79px]">
+        <div className="board-desc flex flex-wrap gap-[14px] md:gap-[26px] justify-center w-full my-[50px] md:my-[79px]">
           <Description info="Responses" number={120} icon={bookimg} />
           <Description info="CREATED ASSESSMENTS" number={12} icon={booksaved} />
           <Description info="PASS/FAIL RATIO" number={'3:1'} icon={ratioimg} />
         </div>
-        <div className="search w-full border-[#E1E3E2] border-[1px] p-4 rounded-lg flex items-center">
+        <div className="search w-full border-[#E1E3E2] border-[1px] p-2 md:p-4 rounded-lg flex items-center">
           <div className="icon relative w-4 h-4 mr-2">
             <Image src={searchimg} alt="search" layout="fill" />
           </div>
