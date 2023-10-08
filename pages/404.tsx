ce15db0c0f7a35f56e2b9a0604a73c1f0d6d989b
Ezeh20 <ranter1570@gmail.com>
2023-10-08 10:16:15 +0100

    reverted the hard coded colors

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,7 +15,7 @@ function Error404() {
           <Image src={logo} alt="logo" />
         </Link>
       </header>
-      <div className=" border-b border-[#EBEEEF] border-style: solid" />
+      <div className=" border-b border-white-115 border-style: solid" />
       <section
         className=" flex flex-col gap-9 sm:gap-[72px] min-h-[calc(100vh-105px)] max-w-[1240px] 
       mx-6 sm:mx-[105] xl:mx-auto items-center justify-center xl:flex-row-reverse xl:gap-[162px]"
@@ -29,7 +29,7 @@ function Error404() {
           <h2 className=" text-2xl md:text-[32px] xl:text-[45px] md:leading-[36px] xl:leading-[52px] sm:font-bold xl:text-left max-w-[504px] text-center font-semibold font-manropeL xl:font-manropeB">
             {title}
           </h2>
-          <p className=" text-[#737876] max-w-[623px]  text-sm sm:text-2xl sm:left-8 sm:text-[#8D9290] text-center xl:text-left ">
+          <p className="text-white-650 max-w-[623px]  text-sm sm:text-2xl sm:left-8 sm:text-gray-300 text-center xl:text-left ">
             {text}
           </p>
           <Button className=" w-full h-[52px] xl:w-[517px] rounded-lg text-base mt-3" href="/">
