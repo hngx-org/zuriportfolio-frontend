97fef1d0cf2076091e4496fb6a8c06190e05e569
Supremacy <musaabdulmuqaddas356@gmail.com>
2023-10-07 23:13:40 -0500

    Recreated components in a new repo

---
+++ modules/super-admin/analytics-components/AnalyticsAndReportingGraphs.tsx
@@ -0,0 +1,221 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+type Graph = {
+  id: number;
+  title: string;
+  btn: string;
+  calender: {
+    twelveM: string;
+    threeM: string;
+    thirtyD: string;
+    sevenD: string;
+    twentyFourH: string;
+    md: boolean;
+  };
+};
+
+type activity = {
+  name: string;
+  purchased: string;
+  pItem: string;
+  id: number;
+};
+
+const AnalyticsAndReportingGraphs = () => {
+const [isGraph, setIsGraph] = useState (false)
+
+useEffect(() => {
+  if (window.innerWidth > 768) {
+    setIsGraph(true);
+  }
+}, []);
+
+
+  const graphDetails: Graph[] = [
+    {
+      id: 1,
+      title: 'Sales report',
+      btn: 'View report',
+      calender: {
+        twelveM: '12 mon',
+        threeM: '3 mon',
+        thirtyD: '30 days',
+        sevenD: '7 days',
+        twentyFourH: '24 hrs',
+        md: true,
+      },
+     
+    },
+    {
+      id: 2,
+      title: 'Sales',
+      btn: 'View report',
+      calender: {
+        twelveM: '12 mon',
+        threeM: '3 mon',
+        thirtyD: '30 days',
+        sevenD: '7 days',
+        twentyFourH: '24 hrs',
+        md: true,
+      },
+    },
+  ];
+
+  const activityDetails: activity[] = [
+    {
+      name: 'Demi Wikinson',
+      purchased: 'Purchased',
+      pItem: 'Webflow101',
+      id: 1,
+    },
+    {
+      name: 'John Doe',
+      purchased: 'Purchased',
+      pItem: 'ProductX',
+      id: 2,
+    },
+    {
+      name: 'Jane Smith',
+      purchased: 'Purchased',
+      pItem: 'ServiceY',
+      id: 3,
+    },
+    {
+      name: 'Bob Johnson',
+      purchased: 'Purchased',
+      pItem: 'ProductZ',
+      id: 4,
+    },
+    {
+      name: 'Alice Brown',
+      purchased: 'Purchased',
+      pItem: 'ServiceA',
+      id: 5,
+    },
+    {
+      name: 'Charlie Davis',
+      purchased: 'Purchased',
+      pItem: 'ProductB',
+      id: 6,
+    },
+    {
+      name: 'Eva White',
+      purchased: 'Purchased',
+      pItem: 'ServiceC',
+      id: 7,
+    },
+    {
+      name: 'Frank Miller',
+      purchased: 'Purchased',
+      pItem: 'ProductD',
+      id: 8,
+    },
+    {
+      name: 'Grace Wilson',
+      purchased: 'Purchased',
+      pItem: 'ServiceE',
+      id: 9,
+    },
+    {
+      name: 'Grace Wilson',
+      purchased: 'Purchased',
+      pItem: 'ServiceE',
+      id: 10,
+    },
+    {
+      name: 'Grace Wilson',
+      purchased: 'Purchased',
+      pItem: 'ServiceE',
+      id: 11,
+    },
+  ];
+
+  const chartData = [
+    { name: 'January', uv: 4000, pv: 2400, amt: 2400 },
+    { name: 'February', uv: 3000, pv: 1398, amt: 2210 },
+    { name: 'March', uv: 2000, pv: 9800, amt: 2290 },
+    { name: 'April', uv: 2780, pv: 3908, amt: 2000 },
+    { name: 'May', uv: 1890, pv: 4800, amt: 2181 },
+    { name: 'June', uv: 2390, pv: 3800, amt: 2500 },
+    { name: 'July', uv: 3490, pv: 4300, amt: 2100 },
+    { name: 'August', uv: 4000, pv: 2400, amt: 2400 },
+    { name: 'September', uv: 3000, pv: 1398, amt: 2210 },
+    { name: 'October', uv: 2000, pv: 9800, amt: 2290 },
+    { name: 'November', uv: 2780, pv: 3908, amt: 2000 },
+    { name: 'December', uv: 1890, pv: 4800, amt: 2181 },
+  ];
+
+  return (
+    <>
+      <section className="my-10 mx-auto px-6 font-sans gap-2 space-y-6  md:max-w-[1270px] md:space-y-0 lg:flex lg:justify-between lg:items-center ">
+        <div className="space-y-6 md:space-y-0  md:flex-grow">
+          {graphDetails.map((item, index) => (
+            <div key={item.id} className="mb-8 lg:w-[98%]">
+              <div className=" shadow-sm rounded-sm border border-white-200 bg-white-100">
+                <div className="flex justify-between items-center px-6 mt-5 text-[#2E3130]">
+                  <h3 className="text-[18px] font-light">{item.title}</h3>
+                  <button className="font-light rounded-lg border border-white-100 py-2.5 px-4 text-[15px] md:bg-brand-green-primary md:text-white-100 md:border-0">
+                    {item.btn}
+                  </button>
+                </div>
+                <div className="flex justify-center items-center font-light text-[#8D9290] px-6 mt-8 md:justify-between">
+                  <div className="flex justify-center items-center text-center  gap-3 text-[13px] sm:text-[15px] md:gap-5">
+                    <p className="rounded-md bg-green-300 bg-opacity-20 py-1.5 px-2 text-green-900">
+                      {isGraph ? '12 months' : item.calender.twelveM }
+                    </p>
+                    <p>{isGraph ? '3 months' : item.calender.threeM }</p>
+                    <p>{item.calender.thirtyD}</p>
+                    <p>{item.calender.sevenD}</p>
+                    <p>{isGraph ? '24 hours' : item.calender.twentyFourH}</p>
+                  </div>
+                  {index === 1 && (
+                    <div className="hidden md:flex gap-3 text-[15px]">
+                      <p>Sales</p>
+                      <p>Order</p>
+                      <p>Users</p>
+                    </div>
+                  )}
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <ResponsiveContainer height={230} className="mx-auto mt-8 -z-20">
+                    <LineChart data={chartData}>
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                      <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                      <Tooltip wrapperStyle={{zIndex: 1000}}  />
+                      <Legend />
+                      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                    </LineChart>
+                  </ResponsiveContainer>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <section className="lg:w-[25%]">
+          <div className="py-7 px-6 sm:px-10 lg:shadow-sm bg-white-100 lg:border-white-200 lg:border lg:rounded-lg">
+            <div className="flex justify-between items-center mb-8">
+              <h3 className="text-[19px]">Activity</h3>
+              <p className="text-[#5B5F5E] text-[15px]">View All</p>
+            </div>
+            <div className="space-y-5 md:space-y-[13.5px]">
+              {activityDetails.map((item) => (
+                <div key={item.id}>
+                  <h3 className="text-[#5B5F5E] text-[17px]">{item.name}</h3>
+                  <div className="flex gap-2">
+                    <p className="text-[#737876] font-light">{item.purchased}</p>
+                    <span className="text-[#F1AE67]">{item.pItem}</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default AnalyticsAndReportingGraphs;
---
+++ modules/super-admin/analytics-components/AnalyticsAndReportingTopSelling.tsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import Image from 'next/image';
+import SuperAdminPagination from '../components/pagination';
+
+type Product = {
+  id: number;
+  productName: string;
+  productImage: string;
+  category: string;
+  order: string;
+  price: string;
+  topSales: string;
+  vendor: string;
+};
+
+type SuperAdminPaginationProps = {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+  title?: string | JSX.Element; // Make title optional
+};
+
+const AnalyticsAndReportingTopSelling = () => {
+  const products: Product[] = [
+    {
+      id: 1,
+      productName: 'Product Design course',
+      productImage: '/assets/tsImages/image 10.png',
+      category: 'Tech Learning',
+      order: '123456',
+      price: '$20',
+      topSales: '$1558767',
+      vendor: '/assets/tsImages/more.png',
+    },
+    {
+      id: 2,
+      productName: 'Full Javascript Course',
+      productImage: '/assets/tsImages/image 12.png',
+      category: 'Tech Learning',
+      order: '113456',
+      price: '$22',
+      topSales: '$1262621',
+      vendor: '/assets/tsImages/more.png',
+    },
+    {
+      id: 3,
+      productName: 'Data Analics Course',
+      productImage: '/assets/tsImages/image 10.png',
+      category: 'Tech Learning',
+      order: '103456',
+      price: '$17',
+      topSales: '$1178882',
+      vendor: '/assets/tsImages/more.png',
+    },
+    {
+      id: 4,
+      productName: 'HTML & CSS Course',
+      productImage: '/assets/tsImages/image 9.png',
+      category: 'Tech Learning',
+      order: '93456',
+      price: '$19',
+      topSales: '$976351',
+      vendor: '/assets/tsImages/more.png',
+    },
+    {
+      id: 5,
+      productName: 'Python complete Course',
+      productImage: '/assets/tsImages/image 12.png',
+      category: 'Tech Learning',
+      order: '92456',
+      price: '$28',
+      topSales: '$1287873',
+      vendor: '/assets/tsImages/more.png',
+    },
+    {
+      id: 6,
+      productName: 'Product Management Course',
+      productImage: '/assets/tsImages/image 9.png',
+      category: 'Tech Learning',
+      order: '89456',
+      price: '$34',
+      topSales: '$1977637',
+      vendor: '/assets/tsImages/more.png',
+    },
+    {
+      id: 7,
+      productName: 'Digital marketing Course',
+      productImage: '/assets/tsImages/image 10.png',
+      category: 'Tech Learning',
+      order: '86456',
+      price: '$26',
+      topSales: '$1456421',
+      vendor: '/assets/tsImages/more.png',
+    },
+ 
+  ];
+
+  const productName =  <div className='flex items-center gap-1'>
+  <span>Product Name </span>
+   <Image src="/assets/tsImages/arrow-down.png" alt='Product Icon' width={20}  height={20} className='object-contain'/>
+   </div>;
+
+const currentPage = 1;
+const totalPages = 10;
+
+   return (
+    <section className=" px-4 mb-10 font-sans">
+      <div className="max-w-[1220px] mx-auto py-4 border border-white-200 rounded-lg shadow-md overflow-x-auto">
+        <div className="grid grid-cols-2 min-w-[1000px] items-center text-[#667085] border-b border-white-200 px-4 py-3 bord">
+          <div className="flex items-center gap-1">
+            <span className="md:pl-8">Product Name </span>
+            <Image src="/assets/tsImages/arrow-down.png" alt="Product Icon" width={20} height={20} className="object-contain" />
+          </div>
+          <div className="grid grid-cols-5 text-center min-w-[100px]">
+            <span>Category</span>
+            <span>Order</span>
+            <span>Price</span>
+            <span>Top Sales</span>
+            <span>Vendor</span>
+          </div>
+        </div>
+        <div className="min-w-[1000px]">
+          {products.map((product) => (
+            <div key={product.id} className="grid grid-cols-2 items-center border-b border-white-200 shadow-sm bg-white-100 py-4 px-4 whitespace-nowrap">
+              <div className="flex items-center md:pl-8 ">
+                <Image src={product.productImage} alt={product.productName} width={50} height={50} />
+                <span className="ml-4 text-md md:text-lg">{product.productName}</span>
+              </div>
+              <div className="grid grid-cols-5 text-[#667085] text-center min-w-[100px]">
+                <p className="">{product.category}</p>
+                <p>{product.order}</p>
+                <p>{product.price}</p>
+                <p>{product.topSales}</p>
+                <Image src={product.vendor} alt={product.productName} width={20} height={20} className='ml-12' />
+              </div>
+            </div>
+          ))}
+        </div>
+        <SuperAdminPagination title="Pagination Title" currentPage={currentPage} totalPages={totalPages} onPageChange={() => {}} />
+      </div>
+    </section>
+  );
+};
+
+export default AnalyticsAndReportingTopSelling;
