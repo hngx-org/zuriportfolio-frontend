28ce81adeac153260fc2a03b1e03cd5b31ed5906
IbiminaFG <gogoibimina@gmail.com>
2023-10-08 20:59:29 +0100

    Finished the scorig screen

--- modules/assessment/component/scoreDropdown.tsx
+++ modules/assessment/component/scoreDropdown.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import React, { useState, useEffect, useRef } from 'react';
 
 interface ScoreDropdownProps {
@@ -6,89 +5,29 @@ interface ScoreDropdownProps {
 }
 
 const ScoreDropdown = (props: { item: string }) => {
-  const [selectedOption, setSelectedOption] = useState('');
-  const [isOpen, setIsOpen] = useState(false);
-  const modalRef = useRef<HTMLDivElement | null>(null);
-
-  const handleOptionSelect = (option: string) => {
-    setSelectedOption(option);
-    setIsOpen(false);
-  };
-
-  const handleModalToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  const handleIconClick = (e: React.MouseEvent<HTMLImageElement>) => {
-    e.stopPropagation(); // Prevent click event propagation
-    handleModalToggle();
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      const handleOutsideClick = (e: MouseEvent) => {
-        if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
-          closeModal();
-        }
-      };
-
-      window.addEventListener('click', handleOutsideClick);
-
-      return () => {
-        window.removeEventListener('click', handleOutsideClick);
-      };
-    }
-  }, [isOpen]);
-
   return (
-    <div className="flex flex-col sm:flex-row sm:items-center mt-4 w-full">
-      <p className="sm:w-[112px] text-base font-manropeL mb-1">{props.item}</p>
-      <div className="flex gap-5">
-        <div className="relative border border-[#BFC8CC] border-solid rounded-xl flex items-center pl-[79px] pr-[16px] py-1">
-          <input type="text" name="score" id="score" value={selectedOption} className="outline-none w-full" readOnly />
-          <Image
-            src="/assets/arrow-down.svg"
-            alt="arrow down icon"
-            width={20}
-            height={20}
-            className="cursor-pointer"
-            onClick={handleIconClick}
-          />
-          {isOpen && (
-            <div
-              ref={modalRef}
-              className="overflow-hidden text-[10px] z-50 absolute right-0 bottom-[-112px] w-[74px] h-28 flex justify-evenly flex-col items-center rounded-lg bg-[#F9F9F9] border border-solid border-[#A8ACAB]"
-            >
-              <div
-                className={`hover:bg-[#CAEAD4] cursor-pointer h-full w-full flex justify-center items-center ${
-                  selectedOption === '0% - 49%' ? 'bg-[#CAEAD4]' : ''
-                }`}
-                onClick={() => handleOptionSelect('0% - 49%')}
-              >
-                0% - 49%
-              </div>
-              <div
-                className={`hover:bg-[#CAEAD4] cursor-pointer h-full w-full flex justify-center items-center ${
-                  selectedOption === '50% - 79%' ? 'bg-[#CAEAD4]' : ''
-                }`}
-                onClick={() => handleOptionSelect('50% - 79%')}
-              >
-                50% - 79%
-              </div>
-              <div
-                className={`hover.bg-[#CAEAD4] cursor-pointer h-full w-full flex justify-center items-center ${
-                  selectedOption === '80% - 100%' ? 'bg-[#CAEAD4]' : ''
-                }`}
-                onClick={() => handleOptionSelect('80% - 100%')}
-              >
-                80% - 100%
-              </div>
-            </div>
-          )}
+    <div>
+      <div className="flex items-center">
+        <h3 className="text-base text-gray-500 w-32">{props.item}:</h3>
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-2">
+            <span className="text-xs text-gray-400">Min Score</span>
+            <input
+              type="number"
+              name="min"
+              id="min"
+              className="text-sm text-center rounded-md border border-solid border-gray-100 w-14 py-2 px-4 outline-none"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="text-xs text-gray-400">Max Score</span>
+            <input
+              type="number"
+              name="max"
+              id="max"
+              className="text-sm text-center rounded-md border border-solid border-gray-100 w-14 py-2 px-4 outline-none"
+            />
+          </div>
         </div>
       </div>
     </div>
--- modules/assessment/scoringScreen.tsx
+++ modules/assessment/scoringScreen.tsx
@@ -5,43 +5,11 @@ import Header from './component/Header';
 import Link from 'next/link';
 
 const ScoringScreen = () => {
-  const arr = ['Intermediate', 'Expert'];
+  const arr = ['Beginner', 'Intermediate', 'Expert'];
   return (
     <div>
-      <Header heading={'Preview/Edit'} body={'An overview of all questions and answers.'} />
-      <div>
-        <div className="flex items-center justify-between px-12 py-6 md:px-24 md:py-8">
-          <Link href={'/'} className="hover:underline flex items-center gap-1">
-            <Image src="/assets/arrow-left.svg" alt="arro left icon" width={20} height={20} color="#000" />
-            Go back
-          </Link>
-          <button className="rounded-2xl text-white-300 px-3 py-2 text-xs sm:text-lg sm:py-3 sm:px-5 bg-green-600">
-            Save Changes
-          </button>
-        </div>
-
-        <div className=" border-b border-gray-500 pb-0">
-          <div className="flex justify-center items-center gap-12">
-            <Link href={'/assessment/create-assessment'}>
-              <button className="font-bold text-gray-400 font-manropeL">Questions & Answer</button>
-            </Link>
-            <Link href={'/assessment/scoring'}>
-              <button
-                style={{
-                  color: 'rgba(191, 132, 67, 1)',
-                  borderBottom: '4px solid rgba(191, 132, 67, 1)',
-                  borderRadius: '2px',
-                }}
-                className="font-manropeL"
-              >
-                Scoring
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="py-14 flex justify-center items-center w-full">
-        <form action="" className="px-6 flex flex-col items-center gap-[45px] w-full">
+      <div className="flex justify-center items-center w-full">
+        <form action="" className="text-left px-6 flex flex-col items-center gap-[45px] w-full">
           {/* Badge Option */}
 
           <div className="w-full rounded-tr-2xl rounded-tl-2xl overflow-hidden sm:w-[604px] border border-[#DFE3E6] border-solid">
@@ -79,26 +47,15 @@ const ScoringScreen = () => {
 
           {/* Grading System */}
           <div className="w-full rounded-tr-2xl rounded-tl-2xl overflow-hidden h-[412px] sm:w-[604px] border border-[#DFE3E6] border-solid">
-            <div className="text w-full h-5 bg-[#BF8443]"></div>
+            <div className="w-full h-5 bg-[#BF8443]"></div>
             <div className="mt-5 mb-7 px-3 sm:px-14">
               <h3 className="text-2xl text-[#191C1E] font-manropeB mb-8">Grading System</h3>
               <p className="text-[#191C1E] text-4 font-manropeB mb-4">Set the scoring range for issuing badges</p>
-              <div className="flex flex-col sm:flex-row sm:items-center">
-                <p className="w-[112px] text-base font-manropeL mb-1">Beginner</p>
-                <div className="flex gap-5">
-                  <input
-                    type="number"
-                    className="border border-solid border-[#BFC8CC] rounded-xl outline-none px-5 py-1 w-full sm:w-[149px]"
-                  />
-                  <input
-                    type="number"
-                    className="border border-solid border-[#BFC8CC] rounded-xl outline-none px-5 py-1 w-full sm:w-[149px]"
-                  />
-                </div>
+              <div className="flex flex-col gap-5">
+                {arr.map((item, index) => (
+                  <ScoreDropdown key={index} item={item} />
+                ))}
               </div>
-              {arr.map((item, index) => (
-                <ScoreDropdown key={index} item={item} />
-              ))}
             </div>
           </div>
         </form>
