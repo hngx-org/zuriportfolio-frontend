e41d73ea0310ffc147fea147b55643f579dfaffd
Timothy Akobundu <timothy.christian10@gmail.com>
2023-10-08 21:15:05 +0100

    ready

--- modules/super-admin/components/product-listing-navbar/ProductListingNavbar.tsx
+++ modules/super-admin/components/product-listing-navbar/ProductListingNavbar.tsx
@@ -1,67 +1,5 @@
-// import { ArrowUp } from 'iconsax-react';
-
-// const ProductsListingNavbar = () => {
-//   return (
-//     <div className="max-w-7xl mx-auto">
-//       {/* product listing Nav bar starts */}
-//       <div className="flex justify-between items-center">
-//         <div>
-//           <div className="mx-auto">
-//             <div className="w-96 border-2 border-custom-color1 py-5 px-5 h-32 rounded-lg ">
-//               <p className="text-xl font-medium">Total Products</p>
-//               <div className="flex justify-between items-center mt-1">
-//                 <h3 className="text-3xl font-bold ">259</h3>
-//                 <div className="flex items-center bg-green-50 rounded-2xl p-2  h-5">
-//                   <ArrowUp size={18} className="text-green-300" />
-//                   <p className="text-green-300 text-sm">10%</p>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div>
-//           <div className="mx-auto">
-//             <div className="w-96 border-2 border-custom-color1 py-5 px-5 h-32 rounded-lg">
-//               <p className="text-xl font-medium">Sanctioned Products</p>
-//               <div className="flex justify-between items-center mt-1">
-//                 <h3 className="text-3xl font-bold ">14</h3>
-//                 <div className="flex items-center">
-//                   <button className="text-xl bg-green-700  text-white-100 rounded-3xl px-4 py-2 h-8 flex items-center justify-center">
-//                     {' '}
-//                     View
-//                   </button>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div>
-//           <div className="mx-auto">
-//             <div className="w-96 border-2  border-custom-color1 py-5 px-5 h-32 rounded-lg">
-//               <p className="text-xl font-medium">Delited Products</p>
-//               <div className="flex justify-between items-center mt-1">
-//                 <h3 className="text-3xl font-bold ">14</h3>
-//                 <div className="flex items-center">
-//                   <button className="text-xl bg-green-700  text-white-100 rounded-3xl px-4 py-2 h-8 flex items-center justify-center">
-//                     {' '}
-//                     View
-//                   </button>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       {/* product listing Nav bar ends */}
-//     </div>
-//   );
-// };
-
-// export default ProductsListingNavbar;
-
 import { ArrowUp } from 'iconsax-react';
+import Link from 'next/link';
 
 const ProductsListingNavbar = () => {
   return (
@@ -91,8 +29,7 @@ const ProductsListingNavbar = () => {
                 <h3 className="text-3xl font-bold">14</h3>
                 <div className="flex items-center">
                   <button className="text-xl bg-green-700 text-white-100 rounded-3xl px-4 py-2 h-8 flex items-center justify-center">
-                    {' '}
-                    View
+                    <Link href="/product-listing/sanctioned-products"> View</Link>
                   </button>
                 </div>
               </div>
@@ -108,8 +45,7 @@ const ProductsListingNavbar = () => {
                 <h3 className="text-3xl font-bold">14</h3>
                 <div className="flex items-center">
                   <button className="text-xl bg-green-700 text-white-100 rounded-3xl px-4 py-2 h-8 flex items-center justify-center">
-                    {' '}
-                    View
+                    <Link href="/ product-listing/deleted-products"> View</Link>
                   </button>
                 </div>
               </div>
--- modules/super-admin/components/product-listing-table/ProductListingTable.tsx
+++ modules/super-admin/components/product-listing-table/ProductListingTable.tsx
@@ -1,74 +1,121 @@
+'use client';
 // import { useState } from 'react';
-import React, { FC } from 'react';
-import { More } from 'iconsax-react';
+// import React, { FC, useState} from 'react';
+import React, { useState } from 'react';
+
+import { More, Sort } from 'iconsax-react';
 import { ProductInfo } from '../../../../@types';
 import SearchProduct from '@modules/super-admin/components/product-listing/searchProduct';
 import FilterProduct from '@modules/super-admin/components/product-listing/filterProduct';
+import Button from '@ui/Button';
+import Link from 'next/link';
 
-const data: ProductInfo[] = [
-  {
-    productName: 'Learning Design',
-    vendor: 'Okereke James',
-    id: 1234,
-    dateAdded: '08-01-23',
-    status: 'Sanctioned',
-  },
-  {
-    productName: 'Frontend development ',
-    vendor: 'Chukwu Chinaza',
-    id: 1235,
-    dateAdded: '08-01-23',
-    status: 'Deleted',
-  },
-  {
-    productName: 'Backend development',
-    vendor: 'Mark Essien',
-    id: 1236,
-    dateAdded: '08-01-23',
-    status: 'Active',
-  },
-  {
-    productName: 'Ethical Hacking',
-    vendor: 'Shobande Abraham',
-    id: 1237,
-    dateAdded: '08-01-23',
-    status: 'Active',
-  },
-  {
-    productName: 'Product management ',
-    vendor: 'John Paul',
-    id: 1238,
-    dateAdded: '08-01-23',
-    status: 'Active',
-  },
-  {
-    productName: 'Digital Marketing',
-    vendor: 'Bakare Femi',
-    id: 1239,
-    dateAdded: '08-01-23',
-    status: 'Active',
-  },
-  {
-    productName: 'Fullstack development ',
-    vendor: 'Gustavo Silas',
-    id: 1233,
-    dateAdded: '08-01-23',
-    status: 'Active',
-  },
-];
+const ProductListingTable = () => {
+  const data: ProductInfo[] = [
+    {
+      productName: 'Learning Design',
+      vendor: 'Okereke James',
+      id: 1234,
+      dateAdded: '08-01-23',
+      status: 'Sanctioned',
+    },
+    {
+      productName: 'Frontend Development',
+      vendor: 'Chukwu Chinaza',
+      id: 1235,
+      dateAdded: '08-02-23',
+      status: 'Deleted',
+    },
+    {
+      productName: 'Backend Development',
+      vendor: 'Mark Essien',
+      id: 1236,
+      dateAdded: '08-03-23',
+      status: 'Active',
+    },
+    {
+      productName: 'Ethical Hacking',
+      vendor: 'Shobande Abraham',
+      id: 1237,
+      dateAdded: '08-04-23',
+      status: 'Active',
+    },
+    {
+      productName: 'Product Management',
+      vendor: 'John Paul',
+      id: 1238,
+      dateAdded: '08-05-23',
+      status: 'Active',
+    },
+    {
+      productName: 'Digital Marketing',
+      vendor: 'Bakare Femi',
+      id: 1239,
+      dateAdded: '08-06-23',
+      status: 'Active',
+    },
+    {
+      productName: 'Fullstack Development',
+      vendor: 'Gustavo Silas',
+      id: 1233,
+      dateAdded: '08-07-23',
+      status: 'Active',
+    },
+  ];
+  const [searchVal, setSearchVal] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(data);
 
-const ProductListingTable: FC = () => {
+  const handleSearch = (searchText: string) => {
+    const filteredProduct: Array<{
+      productName: string;
+      vendor: string;
+      id: number;
+      dateAdded: string;
+      status: string;
+    }> = data.filter((product) => product.productName.toLowerCase().includes(searchText.toLowerCase()));
+    setSearchVal(searchText);
+    setFilteredProducts(filteredProduct);
+  };
+
+  const handleFilter = (status: string) => {
+    const sortedProducts = [...data].sort((a, b) => {
+      const dateA = new Date(a.dateAdded);
+      const dateB = new Date(b.dateAdded);
+
+      if (status === 'newest') {
+        return dateB.getTime() - dateA.getTime(); // Newest to oldest
+      } else {
+        return dateA.getTime() - dateB.getTime(); // Oldest to newest
+      }
+    });
+    setFilteredProducts(sortedProducts);
+  };
+
   return (
-    <div className="font-manropeL max-w-7xl mx-auto">
+    <div className="font-manropeL max-w-7xl mx-auto border-2 border-custom-color1 mt-4">
       {/* Heading */}
-      <div>
+      <div className="flex  justify-between items-center p-4">
         <div className="py-2">
           <h2 className="text-lg  text-gray-900  font-bold">Products Listing</h2>
           <p className="text-gray-500 text-base">List of all products and their details</p>
         </div>
+
+        <div className="flex justify-between items-center gap-2">
+          <SearchProduct handleSearchChange={handleSearch} />
+          <div>
+            <div className="md:block hidden">
+              <FilterProduct handleFilter={handleFilter} />
+            </div>
+            <div className="md:hidden block">
+              <Button intent={'primary'} size={'sm'}>
+                <Sort />
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
       {/* Deleted products list */}
-      <table className="w-full border-2 border-custom-color1 ">
+      <table className="w-full ">
         <thead>
           {/* Table Headers */}
           <tr>
@@ -93,11 +140,14 @@ const ProductListingTable: FC = () => {
         </thead>
         <tbody>
           {/* Listed Products */}
-          {data.map((product, index) => (
+          {filteredProducts.map((product, index) => (
             <tr className="border-t border-custom-color1 border-2 py-4" key={index}>
               <td className="tracking-wide font-manropeL text-base text-gray-900 px-6 py-6 items-center gap-6 self-stretch flex ">
                 <input type="checkbox" />
-                <p>{product.productName}</p>
+                <Link href="/product-listing/product-details">
+                  {' '}
+                  <p>{product.productName} </p>
+                </Link>
               </td>
               <td className="tracking-wide font-manropeL text-base text-gray-900 px-6 py-6 text-center">
                 <p>{product.vendor} </p>
