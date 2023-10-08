c74dbd03e5e8575c92bfe8bd2e2e2469610e2786
Meyangz <Kayfolayan614@gmail.com>
2023-10-08 11:52:27 +0100

    created new branch/ deleted old branch/some changes

--- modules/marketplace/component/CategoriesNav/CategoriesNav.tsx
+++ modules/marketplace/component/CategoriesNav/CategoriesNav.tsx
@@ -9,14 +9,20 @@ const CategoriesNav = (props: CategoriesNavProps) => {
 
   return (
     <ul className="hidden lg:flex gap-8 py-5 mb-8 xl:mb-[52px] relative max-w-[1240px] mx-auto z-50 text-sm xl:text-base">
-      <li>All Categories</li>
+      <li className="whitespace-nowrap">All Categories</li>
       {navItems.map((item) => {
         return (
           <li
             key={item.toLowerCase()}
             className=" font-ppReg text-base  hover:text-brand-green-shade50 select-none hover:cursor-pointer transition duration-400 ease-in-out text-brand-green-shade10 font-normal"
           >
-            {item ? <a href="">{item}</a> : item}
+            {item ? (
+              <a className="whitespace-nowrap" href="">
+                {item}
+              </a>
+            ) : (
+              item
+            )}
           </li>
         );
       })}
