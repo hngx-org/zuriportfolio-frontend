0ebf39d5cc68d5046a55df3d17de0a79447d3a1f
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-10-08 16:28:03 +0100

    i added a useDisclosure to my work

--- pages/view-components/portfolio-certi-award.tsx
+++ pages/view-components/portfolio-certi-award.tsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
-import Button from '@ui/Button';
+import React from 'react';
+import useDisclosure from '../../hooks/useDisclosure'; // Import your custom hook
 import Certifications from '@modules/portfolio/component/certification-modal';
 import Awards from '@modules/portfolio/component/awards-modal';
+import Button from '@ui/Button';
 
 function Tester() {
-  const [isModalOpenForCerti, setIsModalOpenForCerti] = useState(false);
-  const [isModalOpenForAwards, setIsModalOpenForAwards] = useState(false);
+  const { isOpen: isModalOpenForCerti, onOpen: openModalForCerti, onClose: closeModalForCerti } = useDisclosure();
+  const { isOpen: isModalOpenForAwards, onOpen: openModalForAwards, onClose: closeModalForAwards } = useDisclosure();
 
   return (
     <div>
       <div className="flex  flex-col h-screen w-full justify-center items-center gap-24">
-        <Button onClick={() => setIsModalOpenForCerti(true)}>Certification</Button>
-        <Button onClick={() => setIsModalOpenForAwards(true)}>Awards</Button>
+        <Button onClick={openModalForCerti}>Certification</Button>
+        <Button onClick={openModalForAwards}>Awards</Button>
       </div>
-      <Certifications isOpen={isModalOpenForCerti} onClose={() => setIsModalOpenForCerti(false)} />
-      <Awards isOpen={isModalOpenForAwards} onClose={() => setIsModalOpenForAwards(false)} />
+      <Certifications isOpen={isModalOpenForCerti} onClose={closeModalForCerti} />
+      <Awards isOpen={isModalOpenForAwards} onClose={closeModalForAwards} />
     </div>
   );
 }
