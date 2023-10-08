56db9bc0dffd61251993c7831828a82c2bf9d3b7
Timothy Akobundu <timothy.christian10@gmail.com>
2023-10-08 22:33:15 +0100

    chabged a  number

--- modules/super-admin/components/product-listing-navbar/ProductListingNavbar.tsx
+++ modules/super-admin/components/product-listing-navbar/ProductListingNavbar.tsx
@@ -42,7 +42,7 @@ const ProductsListingNavbar = () => {
             <div className="w-96 border-2 border-custom-color1 py-5 px-5 h-32 rounded-lg">
               <p className="text-xl font-medium">Deleted Products</p>
               <div className="flex justify-between items-center mt-1">
-                <h3 className="text-3xl font-bold">14</h3>
+                <h3 className="text-3xl font-bold">15</h3>
                 <div className="flex items-center">
                   <button className="text-xl bg-green-700 text-white-100 rounded-3xl px-4 py-2 h-8 flex items-center justify-center">
                     <Link href="/super-admin/deleted-products"> View</Link>
