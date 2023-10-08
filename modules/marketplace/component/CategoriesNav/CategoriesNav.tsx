34811e019667703ead932769a251615b457801cc
Meyangz <Kayfolayan614@gmail.com>
2023-10-09 00:19:52 +0100

    added functionality/responsiveness

--- modules/marketplace/component/CategoriesNav/CategoriesNav.tsx
+++ modules/marketplace/component/CategoriesNav/CategoriesNav.tsx
@@ -3,18 +3,19 @@ import React, { useState, useEffect } from 'react';
 import ButtonCat from './ButtonCat';
 import SubCategories from './SubCategories';
 import Image from 'next/image';
-// import drop from '../../../../public/assets/'
+import drop from '../../../../public/assets/chevron-down.svg';
 
 interface CategoriesNavProps {
   navItems: string[];
 }
 
 const CategoriesNav = (props: CategoriesNavProps) => {
-  const { navItems } = props;
   const [popupClass, setPopupClass] = useState(false);
   const [active, setActive] = useState(-1);
   const [allCatActive, setAllCatActive] = useState(false);
+  const [showCategories, setShowCategories] = useState(false);
 
+  const { navItems } = props;
   useEffect(() => {
     if (active >= 0) setAllCatActive(false);
   }, [active]);
@@ -24,14 +25,26 @@ const CategoriesNav = (props: CategoriesNavProps) => {
   };
 
   return (
-    <>
-      <div className="max-w-[1240px] mx-auto bg-whte-100 lg:hidden text-sm xl:text-base">
-        <aside>
-          <h3>All Categories</h3>
-          <Image src="" alt="" />
+    <div className=" font-ppReg lg:pt-5  px-4">
+      <div className="max-w-[1240px] absolute  z-40 bg-white-100 right-0 left-0   mx-auto bg-whte-100 lg:hidden text-sm xl:text-base">
+        <aside className="flex justify-between py-5 px-4  ">
+          <h3 className="text-xl">All Categories</h3>
+          <Image
+            onClick={() => setShowCategories((prev) => !prev)}
+            className={`${showCategories ? '-rotate-90' : 'rotate-90'}  z-50`}
+            src={drop}
+            alt="drop icon"
+          />
         </aside>
+        <div className="border-b-[1px]  border-slate-50 w-full lg:hidden"></div>
+        <ul className={` flex-col pb-5 gap-8 mt-8 px-4 ${showCategories ? 'flex' : 'hidden'}`}>
+          <li>All</li>
+          {navItems.map((item, i) => (
+            <li key={i + 1}>{item}</li>
+          ))}
+        </ul>
       </div>
-      <ul className="hidden lg:flex gap-5 xl:gap-8 py-5 mb-8 xl:mb-[52px] relative max-w-[1240px] mx-auto z-50 text-sm xl:text-base">
+      <ul className="hidden whitespace-nowrap lg:flex gap-5 xl:gap-8 py-5   xl:mb-[52px] relative max-w-[1240px] mx-auto z-50 text-sm xl:text-base">
         <li
           className={`${allCatActive ? 'text-brand-green-shade50' : ''}`}
           onClick={() => {
@@ -72,7 +85,7 @@ const CategoriesNav = (props: CategoriesNavProps) => {
           </button>
         </li>
       </ul>
-    </>
+    </div>
   );
 };
 
