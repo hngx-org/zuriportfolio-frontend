bf062c7d635a59ffcc3d22f961e274a142cfbe90
Alom Wisdom <ogwisdom27@gmail.com>
2023-10-09 02:45:25 +0100

    changed my file names

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainLayout from '../../components/Layout/MainLayout';
-import SearchAndFilter from '@modules/portfolio/component/SearchAndFilter/FilterData';
+import SearchAndFilter from '@modules/portfolio/component/SearchAndFilter/SearchAndFilter';
+import Explore from '@modules/portfolio/component/explorePage';
 
 function dashboard() {
   return (
     <MainLayout showTopbar={true} showDashboardSidebar={false} activePage="explore">
       <div>
         <SearchAndFilter />
+        <Explore />
       </div>
     </MainLayout>
   );
