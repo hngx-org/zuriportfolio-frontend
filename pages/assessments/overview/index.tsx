b7627b903c1cbe4c07e413c7b43d5c8f0d5c34a1
B-lovedth <greatsoma2019@gmail.com>
2023-10-09 16:35:29 +0100

    Refactor: modified questions data

--- pages/assessments/overview/index.tsx
+++ pages/assessments/overview/index.tsx
@@ -11,9 +11,7 @@ import { DATA, QuestionType } from '@modules/assessment/mock-data';
 
 function AssessmentOverview() {
   const [questions, setQuestions] = useState(DATA.questions);
-
   const [selectedQuestion, setSelectedQuestion] = useState<QuestionType>();
-
   const [show, setShow] = useState(false);
   const [showConfirm, setShowConfirm] = useState(false);
   const [showSuccessConfirm, setShowSuccessConfirm] = useState(false);
