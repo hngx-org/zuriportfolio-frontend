cd9ee51309629d208f332f2a4e8a046b3ebf8b20
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-10-09 14:57:17 +0100

    adjusted delete modal on mobile view

--- pages/user/customer-purchase-dashboard/index.tsx
+++ pages/user/customer-purchase-dashboard/index.tsx
@@ -163,146 +163,147 @@ const MyPage: React.FC = () => {
   };
 
   return (
-    <MainLayout showFooter showTopbar showDashboardSidebar={false} activePage=''>
-      <div className="px-5 sm:px-16 max-w-screen overflow-hidden">
-        <div className="mt-9 mb-12 hidden sm:block">
-          <div className="flex items-center">
-            <p className="text-base text-brand-green-primary">Settings</p>
-            <span className="mx-[5px]">
-              <ArrowRight2 size="16" color="green" />
-            </span>
-            <p className="text-base text-gray-100">Dashboard</p>
+      <MainLayout showFooter showTopbar showDashboardSidebar={false} activePage=''>
+        <div className="px-5 sm:px-16 max-w-screen overflow-hidden">
+          <div className="mt-9 mb-12 hidden sm:block">
+            <div className="flex items-center">
+              <p className="text-base text-brand-green-primary">Settings</p>
+              <span className="mx-[5px]">
+                <ArrowRight2 size="16" color="green" />
+              </span>
+              <p className="text-base text-gray-100">Dashboard</p>
+            </div>
           </div>
-        </div>
-        <h3 className="font-semibold text-3xl hidden sm:block">Customer Purchase Dashboard</h3>
+          <h3 className="font-semibold text-3xl hidden sm:block">Customer Purchase Dashboard</h3>
 
-        <div className="hidden sm:flex sm:align-center sm:justify-between w-full lg:w-4/5 mt-9">
-          <div
-            className={`h-[2.8rem] w-[12.5rem] flex items-center justify-center border-b-2 border-solid ${
-              filter === null ? 'border-brand-green-primary' : 'border-white-100'
-            }`}
-            onClick={() => handleFilterClick(null)}
-          >
-            <p className={`text-sm lg:text-base ${filter === null ? 'text-brand-green-primary' : 'border-white-100'}`}>
-              All Purchases ({allPurchasesCount})
-            </p>
-          </div>
-          <div
-            className={`h-[2.8rem] w-[12.5rem] flex items-center justify-center border-b-2 border-solid ${
-              filter === 'Pending' ? 'border-brand-green-primary' : 'border-white-100'
-            }`}
-            onClick={() => handleFilterClick('Pending')}
-          >
-            <p className={`text-sm lg:text-base ${filter === 'Pending' ? '' : ''}`}>
-              Pending Purchases ({pendingPurchasesCount})
-            </p>
-          </div>
-          <div
-            className={`h-[2.8rem] w-[12.5rem] flex items-center justify-center border-b-2 border-solid ${
-              filter === 'Successful' ? 'border-brand-green-primary' : 'border-white-100'
-            }`}
-            onClick={() => handleFilterClick('Successful')}
-          >
-            <p
-              className={`text-sm lg:text-base ${
+          <div className="hidden sm:flex sm:align-center sm:justify-between w-full lg:w-4/5 mt-9">
+            <div
+              className={`h-[2.8rem] w-[12.5rem] flex items-center justify-center border-b-2 border-solid ${
+                filter === null ? 'border-brand-green-primary' : 'border-white-100'
+              }`}
+              onClick={() => handleFilterClick(null)}
+            >
+              <p className={`text-sm lg:text-base ${filter === null ? 'text-brand-green-primary' : 'border-white-100'}`}>
+                All Purchases ({allPurchasesCount})
+              </p>
+            </div>
+            <div
+              className={`h-[2.8rem] w-[12.5rem] flex items-center justify-center border-b-2 border-solid ${
+                filter === 'Pending' ? 'border-brand-green-primary' : 'border-white-100'
+              }`}
+              onClick={() => handleFilterClick('Pending')}
+            >
+              <p className={`text-sm lg:text-base ${filter === 'Pending' ? '' : ''}`}>
+                Pending Purchases ({pendingPurchasesCount})
+              </p>
+            </div>
+            <div
+              className={`h-[2.8rem] w-[12.5rem] flex items-center justify-center border-b-2 border-solid ${
                 filter === 'Successful' ? 'border-brand-green-primary' : 'border-white-100'
               }`}
+              onClick={() => handleFilterClick('Successful')}
             >
-              Completed Purchases ({completedPurchasesCount})
-            </p>
-          </div>
-          <div
-            className={`h-[2.8rem] w-[12.5rem] flex items-center justify-center border-b-2 border-solid ${
-              filter === 'Failed' ? 'border-brand-green-primary' : 'border-white-100'
-            }`}
-            onClick={() => handleFilterClick('Failed')}
-          >
-            <p
-              className={`text-sm lg:text-base ${
+              <p
+                className={`text-sm lg:text-base ${
+                  filter === 'Successful' ? 'border-brand-green-primary' : 'border-white-100'
+                }`}
+              >
+                Completed Purchases ({completedPurchasesCount})
+              </p>
+            </div>
+            <div
+              className={`h-[2.8rem] w-[12.5rem] flex items-center justify-center border-b-2 border-solid ${
                 filter === 'Failed' ? 'border-brand-green-primary' : 'border-white-100'
               }`}
+              onClick={() => handleFilterClick('Failed')}
             >
-              Failed Purchases ({failedPurchasesCount})
-            </p>
-          </div>
-        </div>
-
-        <div className="sm:border-r-4 sm:border-white-200  sm:border-solid w-full px-4 flex flex-col gap-8 sm:gap-0">
-          <div className="hidden sm:flex items-center h-[2.5rem] gap-10 mt-[3rem] ">
-            <Input
-              leftIcon={<SearchNormal1 color="#777" />}
-              className="border-2 border-solid border-white-200 pl-6 w-[62.5rem] h-[2.5rem] pr-[1rem] rounded flex-1"
-              placeholder="Search by items, status, seller etc"
-            />
-
-            <FilterDropDown onChooseFilter={onChooseFilter}/>
-
-            <Button
-              onClick={onOpen}
-              className="h-[2.5rem] flex items-center justify-center border-2 border-solid border-white-200 w-[6.25rem] rounded text-red-306 bg-white-100 hover:bg-red-100 hover:border-bg-[#FDCDCD] hover:border-[#FDCDCD] active:border-[#FDCDCD] active:bg-[#FDCDCD] text-[0.88rem]"
-            >
-              <Trash size="16" /> Delete
-            </Button>
-          </div>
-
-          {/* table */}
-          {data.length > 0 && (
-            <div className="hidden sm:block w-full overflow-x-auto">
-              <table className="w-max md:w-full mt-6 mb-8">
-                <thead className="h-[3rem]">
-                  <tr className="bg-white-200">
-                    <th className="text-left px-4 py-2 text-[0.75rem]">
-                      <span className="px-4">
-                        <input type="checkbox" />
-                      </span>
-                      Items
-                    </th>
-                    <th className="text-left px-4 py-2 text-[0.75rem]">Order ID</th>
-                    <th className="text-left px-4 py-2 text-[0.75rem]">Price</th>
-                    <th className="text-left px-4 py-2 text-[0.75rem]">Date</th>
-                    <th className="text-left px-4 py-2 text-[0.75rem]">Sellers Name</th>
-                    <th className="text-left px-4 py-2 text-[0.75rem]">Status</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  {data
-                    .filter((item) => (filter ? item.status.toLowerCase() === filter.toLowerCase() : true))
-                    .map((item) => (
-                      <tr key={item.id} className="border-b border-white-200 border-solid border-1 h-[3.75rem]">
-                        <td className="text-[0.75rem] flex items-center mt-5">
-                          <span className="px-4 ml-[1rem]">
-                            {' '}
-                            <input type="checkbox" />
-                          </span>
-                          {item.item}
-                        </td>
-                        <td className="text-[0.75rem] px-4 py-2">{item.orderID}</td>
-                        <td className="text-[0.75rem] px-4 py-2">{item.price}</td>
-                        <td className="text-[0.75rem] px-4 py-2">{item.date}</td>
-                        <td className="text-[0.75rem] px-4 py-2">{item.sellerName}</td>
-                        <td className="text-[0.75rem] px-4 py-2">
-                          <span
-                            className={`flex items-center justify-center h-[28px] w-[90px] rounded-xl ${
-                              getStatusBackgroundColor(item.status)[0]
-                            }`}
-                          >
-                            <p className={`text-[0.75rem] ${getStatusBackgroundColor(item.status)[1]}`}>{item.status}</p>
-                          </span>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
+              <p
+                className={`text-sm lg:text-base ${
+                  filter === 'Failed' ? 'border-brand-green-primary' : 'border-white-100'
+                }`}
+              >
+                Failed Purchases ({failedPurchasesCount})
+              </p>
             </div>
-          )}
-          <MobileCustomerDashboard />
-          {/* error page */}
-          {data.length === 0 && <PurchaseNotFound />}
+          </div>
+
+          <div className="sm:border-r-4 sm:border-white-200  sm:border-solid w-full px-4 flex flex-col gap-8 sm:gap-0">
+            <div className="hidden sm:flex items-center h-[2.5rem] gap-10 mt-[3rem] ">
+              <Input
+                leftIcon={<SearchNormal1 color="#777" />}
+                className="border-2 border-solid border-white-200 pl-6 w-[62.5rem] h-[2.5rem] pr-[1rem] rounded flex-1"
+                placeholder="Search by items, status, seller etc"
+              />
+
+              <FilterDropDown onChooseFilter={onChooseFilter}/>
+
+              <Button
+                onClick={onOpen}
+                className="h-[2.5rem] flex items-center justify-center border-2 border-solid border-white-200 w-[6.25rem] rounded text-red-306 bg-white-100 hover:bg-red-100 hover:border-bg-[#FDCDCD] hover:border-[#FDCDCD] active:border-[#FDCDCD] active:bg-[#FDCDCD] text-[0.88rem]"
+              >
+                <Trash size="16" /> Delete
+              </Button>
+            </div>
+
+            {/* table */}
+            {data.length > 0 && (
+              <div className="hidden sm:block w-full overflow-x-auto">
+                <table className="w-max md:w-full mt-6 mb-8">
+                  <thead className="h-[3rem]">
+                    <tr className="bg-white-200">
+                      <th className="text-left px-4 py-2 text-[0.75rem]">
+                        <span className="px-4">
+                          <input type="checkbox" />
+                        </span>
+                        Items
+                      </th>
+                      <th className="text-left px-4 py-2 text-[0.75rem]">Order ID</th>
+                      <th className="text-left px-4 py-2 text-[0.75rem]">Price</th>
+                      <th className="text-left px-4 py-2 text-[0.75rem]">Date</th>
+                      <th className="text-left px-4 py-2 text-[0.75rem]">Sellers Name</th>
+                      <th className="text-left px-4 py-2 text-[0.75rem]">Status</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {data
+                      .filter((item) => (filter ? item.status.toLowerCase() === filter.toLowerCase() : true))
+                      .map((item) => (
+                        <tr key={item.id} className="border-b border-white-200 border-solid border-1 h-[3.75rem]">
+                          <td className="text-[0.75rem] flex items-center mt-5">
+                            <span className="px-4 ml-[1rem]">
+                              {' '}
+                              <input type="checkbox" />
+                            </span>
+                            {item.item}
+                          </td>
+                          <td className="text-[0.75rem] px-4 py-2">{item.orderID}</td>
+                          <td className="text-[0.75rem] px-4 py-2">{item.price}</td>
+                          <td className="text-[0.75rem] px-4 py-2">{item.date}</td>
+                          <td className="text-[0.75rem] px-4 py-2">{item.sellerName}</td>
+                          <td className="text-[0.75rem] px-4 py-2">
+                            <span
+                              className={`flex items-center justify-center h-[28px] w-[90px] rounded-xl ${
+                                getStatusBackgroundColor(item.status)[0]
+                              }`}
+                            >
+                              <p className={`text-[0.75rem] ${getStatusBackgroundColor(item.status)[1]}`}>{item.status}</p>
+                            </span>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+            <MobileCustomerDashboard />
+            {/* error page */}
+            {data.length === 0 && <PurchaseNotFound />}
+          </div>
+          {/* delete modal */}
+          <DeleteModal isOpen={isOpen} onClose={onClose} onDelete={onDelete} />
         </div>
-        <DeleteModal isOpen={isOpen} onClose={onClose} onDelete={onDelete} />
-      </div>
-    </MainLayout>
+      </MainLayout>
   );
 };
 
