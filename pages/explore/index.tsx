a397a26cc3b874bf027e5147d64e326c16ef7a8d
Alom Wisdom <ogwisdom27@gmail.com>
2023-10-09 11:10:50 +0100

    Refactor

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainLayout from '../../components/Layout/MainLayout';
 import SearchAndFilter from '@modules/portfolio/component/SearchAndFilter/SearchAndFilter';
+import HomePage from '@modules/portfolio/component/explorePage';
 
 function dashboard() {
   return (
     <MainLayout showTopbar={true} showDashboardSidebar={false} activePage="explore">
       <div>
         <SearchAndFilter />
+        <HomePage />
       </div>
     </MainLayout>
   );
