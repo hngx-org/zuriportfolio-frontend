2bae6bc149ef853a245e752d922d45fe29e4bcca
olafisoyevince <olafisoyevincent@gmail.com>
2023-10-09 14:03:30 +0100

    increased font size on the bold text

--- modules/auth/verificationLinkSent.tsx
+++ modules/auth/verificationLinkSent.tsx
@@ -21,16 +21,15 @@ function VerificationLinkSent({ handleClick }: Props) {
         width={218}
         height={218}
       />
-      <div className=" bg-brand-green-ttr px-4 max-w-[814px] sm:px-[40px] md:px-[58px] lg:px-[148px] py-8 border border-brand-disabled rounded-[32px] z-10">
-        <h1 className=" font-manropeEB text-2xl md:text-4xl text-center">Verification Link Sent</h1>
-        <p className=" font-manropeL text-xs text-center md:w-[90%] text-[#737876] md:text-[#000] py-[16px]  lg:py-[32px]">
+      <div className=" bg-brand-green-ttr px-4 sm:px-[40px] md:mx-[58px] lg:mx-[148px] py-8 border border-brand-disabled rounded-[32px] z-10">
+        <h1 className=" font-manropeEB text-[24px] md:text-[36px] text-center">Verification Link Sent</h1>
+        <p className=" font-manropeL text-[12px] md:text-[16px] text-center md:w-[90%] text-[#737876] md:text-[#000] py-[16px]  lg:py-[32px]">
           We&apos;ve sent an email to your{' '}
-          <span className=" font-manropeEB text-[#003A1B]">www.sunmicah01@gmail.com</span> with a verification link.
+          <span className=" font-manropeEB text-[#003A1B] text-[14px] md:text-[16px]">www.sunmicah01@gmail.com</span>{' '}
+          with a verification link.
         </p>
 
-        <Button size="sm" className=" w-full rounded-md h-[60px]">
-          Resend Verification Link
-        </Button>
+        <Button className=" w-full rounded-md h-[60px] text-[16px] font-manropeB">Resend Verification Link</Button>
 
         <div className=" flex gap-2 flex-col sm:flex-row justify-between pt-3">
           <p className=" font-manropeL text-[10px] text-[#737876] md:text-[#000]">
