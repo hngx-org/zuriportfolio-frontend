1249df207203492d2bdccadfe2a80949abf8cb01
BaZz <bazuaye50@gmail.com>
2023-10-10 03:32:51 +0100

    remove auth desktop menu on mobile

--- components/Navbars/TopBar.tsx
+++ components/Navbars/TopBar.tsx
@@ -190,7 +190,7 @@ function TopBar(props: { activePage: string; showDashBorad: boolean }) {
           {authMenu && (
             <div
               ref={authMenuRef}
-              className="absolute flex flex-col right-0 lg:top-[71px] top-[60px] bg-white-100 shadow-lg z-[60]"
+              className="absolute hidden lg:flex flex-col right-0 lg:top-[71px] top-[60px] bg-white-100 shadow-lg z-[60]"
             >
               <ul>
                 <li className="border-b cursor-pointer hover:bg-[#F4FBF6] border-[#EBEEEF] py-3 px-4 flex gap-3">
