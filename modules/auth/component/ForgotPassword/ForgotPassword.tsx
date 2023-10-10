1ed028e5b165983f4c83276d1ea5b3f55f92f924
olafisoyevince <olafisoyevincent@gmail.com>
2023-10-10 18:57:52 +0100

    fix: made specific changes to the authlayout

--- modules/auth/component/ForgotPassword/ForgotPassword.tsx
+++ modules/auth/component/ForgotPassword/ForgotPassword.tsx
@@ -6,47 +6,44 @@ import { Input } from '@ui/Input';
 const ForgotPassword = () => {
   return (
     <AuthLayout isTopRightBlobShown isBottomLeftPadlockShown={false}>
-      <main className="min-h-screen flex max-w-[1240px] mx-auto lg:py-16 lg:gap-[43px] ">
-        <section className="flex-col flex lg:w-fit w-full lg:text-start text-center ">
-          <div className="font-manropeB flex-1 flex-col  flex justify-center md:py-14  max-w-[517px] mx-auto">
-            <h1 className="font-manropeEB font-bold text-black md:text-[36px] leading-[122.222%] tracking-[-0.09px] md:mb-4 mb-[10px] text-[24px] ">
-              Forgot your Password ?
-            </h1>
-            <p className=" lg:text-[16px] md:text-[22px] text-[14px] font-semibold max-w-[296px] md:max-w-[503px] mx-auto lg:max-w-none lg:mx-0 leading-[150%] tracking-[0.024px] text-custom-color20 md:mb-[70px] mb-10 ">
-              Enter your registered email below to receive reset instructions.
-            </p>
-            <form className="flex flex-col md:gap-12 gap-[30px] md:mb-12 mb-[30px]">
-              <div className="flex flex-col gap-2">
-                <label
-                  htmlFor="email"
-                  className="text-slate-300 text-start font-semibold text-[16px] leading-[169.019%]"
-                >
-                  Email Address
-                </label>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="Aliusugar@gmail.com"
-                  className="w-full px-[18px] py-[13.5px] font-manropeL font-light text-custom-color2  rounded-md border-[1.35px]  border-slate-50"
-                />
-              </div>
-              <Button
-                href="/auth/success"
-                intent={'primary'}
-                className="flex justify-center items-center gap-4 md:pl-[15.0625rem] md:pr-[15.0625rem] py-3 md:px-60 md:w-[100%] w-[100%] h-14 rounded-lg button text-white-100 text-center mt-[1rem]"
-              >
-                Submit
-              </Button>
-            </form>
-            <p className="text-[14px] text-center text-custom-color20 font-medium">
-              Go back to{' '}
-              <Link href="/auth/login" className="text-brand-green-primary">
-                Login
-              </Link>
-            </p>
-          </div>
-        </section>
+      <main className=" mx-auto lg:pt-16 ">
+        {/* <section className="flex-col flex lg:text-start text-center "> */}
+        <div className="font-manropeB text-center lg:text-left pt-36 md:py-14">
+          <h1 className="font-manropeEB font-bold text-black md:text-[36px] leading-[122.222%] tracking-[-0.09px] md:mb-4 mb-[10px] text-[24px] ">
+            Forgot your Password ?
+          </h1>
+          <p className=" lg:text-[16px] md:text-[22px] text-[14px] font-semibold mx-auto lg:max-w-none lg:mx-0 leading-[150%] tracking-[0.024px] text-custom-color20 md:mb-[70px] mb-10 ">
+            Enter your registered email below to receive reset instructions.
+          </p>
+          <form className="flex flex-col md:gap-12 gap-[30px] md:mb-12 mb-[30px]">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="email" className="text-slate-300 text-start font-semibold text-[16px] leading-[169.019%]">
+                Email Address
+              </label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Aliusugar@gmail.com"
+                className="w-full px-[18px] py-[13.5px] font-manropeL font-light text-custom-color2  rounded-md border-[1.35px]  border-slate-50"
+              />
+            </div>
+            <Button
+              href="/auth/success"
+              intent={'primary'}
+              className="py-3 w-full  h-14 rounded-lg button text-white-100"
+            >
+              Submit
+            </Button>
+          </form>
+          <p className="text-[14px] text-center text-custom-color20 font-medium">
+            Go back to{' '}
+            <Link href="/auth/login" className="text-brand-green-primary">
+              Login
+            </Link>
+          </p>
+        </div>
+        {/* </section> */}
       </main>
     </AuthLayout>
   );
