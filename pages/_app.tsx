933ff07e11fd2156de85ab87e3df443ab9103db5
olafisoyevince <olafisoyevincent@gmail.com>
2023-10-11 17:25:37 +0100

    feat: added an auth context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import type { AppProps } from 'next/app';
 import nProgress from 'nprogress';
 import { Router } from 'next/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthContextProvider } from '../context/AuthContext';
 const queryClient = new QueryClient();
 
 // nprogress loader
@@ -34,9 +35,11 @@ export default function App({ Component, pageProps }: AppProps) {
       </style>
       <ErrorBoundary>
         <QueryClientProvider client={queryClient}>
-          <MainLayoutContextProvider>
-            <AnyComponent {...pageProps} />
-          </MainLayoutContextProvider>
+          <AuthContextProvider>
+            <MainLayoutContextProvider>
+              <AnyComponent {...pageProps} />
+            </MainLayoutContextProvider>
+          </AuthContextProvider>
         </QueryClientProvider>
       </ErrorBoundary>
     </>
