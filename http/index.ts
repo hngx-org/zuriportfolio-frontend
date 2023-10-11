bfc901d355d3bcfa38a74507f71c68e78a47f452
Dave <davearonmwan@gmail.com>
2023-10-11 08:47:20 +0100

    created a test login function for users

--- http/index.ts
+++ http/index.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import $http from './axios';
 
 // test
@@ -10,3 +11,23 @@ export const getUserByName = async (props: { name: string }) => {
     return e.response.data ?? { message: e.message };
   }
 };
+
+export const loginUser = async (props: {email: string; password: string}) => {
+  const $http = axios.create({
+    baseURL: "https://auth.akuya.tech",
+    timeout: 30000,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    withCredentials: true,
+  });
+
+  try {
+    const res = await $http.post('/api/auth/login', props);
+    return res?.data;
+  } catch (e: any) {
+    console.log(e);
+    return e.response.data ?? { message: e.message };
+  }
+}
