cc86b7d14cf1d55c64cbc2b04bf66f4c0bb302f0
Benrobo <alumonabenaiah71@gmail.com>
2023-10-11 17:54:19 +0100

    chore: updated axios config

--- http/axios.ts
+++ http/axios.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 // axios config for server
-const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : '';
 const $http = axios.create({
-  baseURL: API_URL,
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
