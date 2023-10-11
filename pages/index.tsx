927cb711c441135664cfe190b9b41638f2f0615c
ASANIYAN <ayomidepaulasaniyan@gmail.com>
2023-10-11 13:14:56 +0100

    code update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link';
 import MainLayout from '../components/Layout/MainLayout';
 
 import ProductDetailsDescription from '../modules/marketplace/productDetailsDescription';
+import Landing from '@modules/portfolio/component/landing-page';
 
 function Home() {
-  return <MainLayout activePage="home" showDashboardSidebar showTopbar></MainLayout>;
+  return <Landing />;
 }
 
 export default Home;
