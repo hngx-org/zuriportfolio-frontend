13533d9889d2bc1a4125c46a9473ad2729dfdcaf
Dave <davearonmwan@gmail.com>
2023-10-11 10:17:24 +0100

    custom auth query hook

---
+++ hooks/Auth/useAuthQuery.ts
@@ -0,0 +1,30 @@
+import { MutationFunction, useMutation, UseQueryOptions, useQuery, QueryKey } from '@tanstack/react-query';
+
+/**
+ * @template TQueryFnData - shape of the data returned from endpoint
+ * @template TError - shape of error returned from endpoint
+ * @template TData - shape of the data returned from endpoint
+ * @template TQueryKey - shape of the query key
+ *
+ * @param {TQueryKey} querykey - the query key
+ * @param {Omit<UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>, 'queryKey' | 'initialData'> & { initialData?: () => undefined; }} options - the options for the query
+ * @returns {UseQueryResult<TData, TError>} - the result of the query
+ */
+
+const useAuthQuery = <
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+>(
+  querykey: TQueryKey,
+  options?: Omit<UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>, 'queryKey' | 'initialData'> & {
+    initialData?: () => undefined;
+  },
+) => {
+  const query = useQuery(querykey, options);
+
+  return query;
+};
+
+export default useAuthQuery;
