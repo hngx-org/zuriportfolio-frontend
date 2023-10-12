4353d7254d37d31d8b4582ecf122e96c81fc83fa
olafisoyevince <olafisoyevincent@gmail.com>
2023-10-12 11:16:45 +0100

    feat: integrated endpoints for the verification page

--- http/index.ts
+++ http/index.ts
@@ -33,6 +33,63 @@ export const loginUser = async (props: { email: string; password: string }) => {
   }
 };
 
+export const verifyUser = async (props: { token: string }) => {
+  const $http = axios.create({
+    baseURL: AUTH_HTTP_URL,
+    timeout: 30000,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  try {
+    const res = await $http.get(`/api/auth/verify/${props.token}`);
+    console.log(res?.data);
+    return res?.data;
+  } catch (e: any) {
+    console.log(e);
+    return e.response.data ?? { message: e.message };
+  }
+};
+
+export const resendEmail = async (props: { token: string }) => {
+  const $http = axios.create({
+    baseURL: AUTH_HTTP_URL,
+    timeout: 30000,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  try {
+    const res = await $http.get(`/api/auth/resend-verification/${props.token}`);
+    console.log(res?.data);
+    return res?.data;
+  } catch (e: any) {
+    console.log(e);
+    return e.response.data ?? { message: e.message };
+  }
+};
+
+export const changeEmail = async (props: { token: string }) => {
+  const $http = axios.create({
+    baseURL: AUTH_HTTP_URL,
+    timeout: 30000,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  try {
+    const res = await $http.get(`/api/auth/verify/${props.token}`);
+    console.log(res?.data);
+    return res?.data;
+  } catch (e: any) {
+    console.log(e);
+    return e.response.data ?? { message: e.message };
+  }
+};
+
 // export const loginUser = async () => {
 //   const $http = axios.create({
 //     baseURL: 'https://reqres.in/',
--- modules/auth/verificationComplete.tsx
+++ modules/auth/verificationComplete.tsx
@@ -1,23 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import VerificationLayout from './component/verificationLayout';
+import useAuthMutation from '../../hooks/Auth/useAuthMutation';
+import { verifyUser } from '../../http';
+import { useRouter } from 'next/router';
 
 function VerificationComplete() {
+  const router = useRouter();
+  const { token } = router.query;
+
+  console.log(token);
+
+  const [message, setMessage] = useState({});
+
+  const { mutate, isLoading } = useAuthMutation(verifyUser, {
+    onSuccess: (data) => {
+      console.log(data);
+
+      setMessage(data);
+
+      router.push('/auth/login');
+    },
+    onError: ({ response }: any) => {
+      console.log(response.data.message);
+
+      if (response.data.message === 'Email already verified. Please login') {
+        router.push('auth/login');
+      } else {
+        router.push('/auth/verification');
+      }
+    },
+  });
+
+  console.log({ message });
+
+  const verifyQuery = async () => {
+    mutate({ token: token as any });
+  };
+
+  useEffect(() => {
+    try {
+      console.log(token, 'inside the try catch');
+      verifyQuery();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [token]);
+
   return (
     <VerificationLayout>
-      <Image
-        className="w-[218px] h-[159px] xl:w-[218px] xl:h-[218px] mx-auto mt-16 md:mt-16 lg:"
-        src="/assets/images/verification-complete.svg"
-        alt=""
-        width={218}
-        height={218}
-      />
-      <div className=" sm:bg-brand-green-ttr px-4 max-w-[712px] sm:px-[40px] md:px-[58px] lg:px-[120px] py-5 sm:border sm:border-brand-disabled rounded-[32px] z-10">
-        <h1 className=" font-manropeEB text-[24px] md:text-[36px] text-center">Verification Complete</h1>
-        <p className=" font-manropeL text-[16px] text-center text-[#737876] md:text-[#000] py-[16px] ">
-          Account verification complete. You will be redirected to a login page.
-        </p>
-      </div>
+      {/* {success && (
+        <>
+          <Image
+            className="w-[218px] h-[159px] xl:w-[218px] xl:h-[218px] mx-auto mt-16 md:mt-16 lg:"
+            src="/assets/images/verification-complete.svg"
+            alt=""
+            width={218}
+            height={218}
+          />
+          <div className=" sm:bg-brand-green-ttr px-4 max-w-[712px] sm:px-[40px] md:px-[58px] lg:px-[120px] py-5 sm:border sm:border-brand-disabled rounded-[32px] z-10">
+            <h1 className=" font-manropeEB text-[24px] md:text-[36px] text-center">Verification Complete</h1>
+            <p className=" font-manropeL text-[16px] text-center text-[#737876] md:text-[#000] py-[16px] ">
+              Account verification complete. You will be redirected to a login page.
+            </p>
+          </div>
+        </>
+      )} */}
+
+      {isLoading && (
+        <div className="w-16 h-16 border-t-4 border-[#009254] border-solid rounded-full animate-spin"></div>
+      )}
+
+      {/* {!success && (
+        <div className=" sm:bg-brand-green-ttr px-4 max-w-[712px] sm:px-[40px] md:px-[58px] lg:px-[120px] py-5 sm:border sm:border-brand-disabled rounded-[32px] z-10">
+          <h1 className=" font-manropeEB text-[24px] md:text-[36px] text-center">Link has expired</h1>
+          <p className=" font-manropeL text-[16px] text-center text-[#737876] md:text-[#000] py-[16px] ">
+            Please get a new link
+          </p>
+        </div>
+      )} */}
     </VerificationLayout>
   );
 }
