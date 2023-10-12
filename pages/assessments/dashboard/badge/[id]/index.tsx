786426c5582e460c39f95898c3f0b1836da42044
peaceOffiong <peaceyben@gamil.com>
2023-10-12 11:05:12 -0700

    fix dataretrieval

--- pages/assessments/dashboard/badge/[id]/index.tsx
+++ pages/assessments/dashboard/badge/[id]/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Badges from '@modules/assessment/component/Badges/Badges';
 import BadgesHeader from '@modules/assessment/component/Badges/BadgesHeader';
 import MainLayout from '../../../../../components/Layout/MainLayout';
@@ -7,13 +7,38 @@ import MainLayout from '../../../../../components/Layout/MainLayout';
 const Page: React.FC = () => {
   const params = useRouter();
   const badgelabel = params.query?.id;
-  console.log(badgelabel);
 
   const [scorePercentage, setScorePercentage] = useState<number>(10);
   const [isdownloadOpen, setIsdownloadOpen] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [badgeData, setBadgeData] = useState([]);
   const onClose = () => {
     setIsdownloadOpen(false);
   };
+
+  useEffect(() => {
+    const apiUrl = `https://demerzel-badges-production.up.railway.app/api/badges/1`;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl, { method: 'GET', redirect: 'follow' });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        setBadgeData(data.badge);
+        console.log(data);
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
