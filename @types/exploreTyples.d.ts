355c3c868093bdd11c96b3bc2eec41c17a602db0
BaZz <bazuaye50@gmail.com>
2023-10-12 17:06:55 +0100

    added user types from api

---
+++ @types/exploreTyples.d.ts
@@ -0,0 +1,16 @@
+export interface UserInfo {
+  address: string;
+  createdAt: string;
+  firstName: string;
+  id: string;
+  lastName: string;
+  location: string;
+  profilePictureUrl: string;
+  profileUrl: string;
+  projects: number;
+  provider: string;
+  ranking: string;
+  skills: string[];
+  tag: string;
+  track: string;
+}
