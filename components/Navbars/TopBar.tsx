96c55597b8d21702798d2f318178e5977d70869a
Allenade <84461901+Allenade@users.noreply.github.com>
2023-10-13 16:33:38 +0100

    Merge branch 'dev' into MAR-26/feat/MoveToCart

--- components/Navbars/TopBar.tsx
+++ components/Navbars/TopBar.tsx
@@ -373,7 +373,7 @@ function TopBar(props: { activePage: string; showDashBorad: boolean }) {
           </div>
         )}
       </nav>
-      <div className="mb-32"></div>
+      <div className="mb-24 md:mb-28 lg:mb-32 "></div>
     </>
   );
 
