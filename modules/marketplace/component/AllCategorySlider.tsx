89bee57986422bb36e870a313c4974509f43ccf0
Benjtalkshow <chinedubenj@gmail.com>
2023-10-13 08:18:42 -0700

    added error handler UI

--- modules/marketplace/component/AllCategorySlider.tsx
+++ modules/marketplace/component/AllCategorySlider.tsx
@@ -4,6 +4,7 @@ import styles from '././landingpage/productCardWrapper/product-card-wrapper.modu
 import Image from 'next/image';
 import Link from 'next/link';
 import axios from 'axios';
+import { error } from 'console';
 
 //I tried including my types in the index.d.ts but it resulted to merge conflict. I had to do it this way
 interface CategoryTypes {
@@ -23,6 +24,7 @@ function AllCategorySlider() {
           setCategories(response.data.categories as CategoryTypes[]);
         } else {
           console.error('Invalid data format in the response.');
+          const err = 'Invalid data format in the response.';
         }
       })
       .catch((error) => {
@@ -33,35 +35,41 @@ function AllCategorySlider() {
   return (
     <div className="allCategoryText  py-10 font-[500] text-xl max-w-[1240px] mx-auto w-full">
       <h1 className="text-[#101928] font-manropeL font-bold md:text-2xl leading-normal">All Categories</h1>
-      <div
-        className={`allCategorySlider  flex flex-nowrap gap-x-3 mt-10 w-full overflow-x-scroll ${styles['hide-scroll']}`}
-      >
-        {categories.map((category, index) => (
-          <Link
-            href={`marketplace/categories/${category.name}`}
-            key={index}
-            className="imageHolder  w-1/2 sm:w-1/3 md:w-1/5 flex-shrink-0 relative cursor-pointer rounded-2xl"
-          >
-            {' '}
-            {
-              <Image
-                width={200}
-                height={200}
-                src="/"
-                alt={category.name}
-                className="w-[100%] h-[100%] object-center object-cover rounded-2xl"
-              />
-            }
-            <p
-              style={{ color: 'white' }}
-              className="text-sm leading-none sm:leading-1 sm:text-[1rem]  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center absolute z-30 p-2  shadow-xl "
+      {categories.length === 0 ? (
+        <div className="py-8 px-4 mt-8 text-center rounded-2xl border border-dark-110/20 text-dark-110 font-manropeL text-xl md:text-2xl font-semibold">
+          No Category To Show
+        </div>
+      ) : (
+        <div
+          className={`allCategorySlider  flex flex-nowrap gap-x-3 mt-10 w-full overflow-x-scroll ${styles['hide-scroll']}`}
+        >
+          {categories.map((category, index) => (
+            <Link
+              href={`marketplace/categories/${category.name}`}
+              key={index}
+              className="imageHolder  w-1/2 sm:w-1/3 md:w-1/5 flex-shrink-0 relative cursor-pointer rounded-2xl"
             >
-              {category.name}
-            </p>
-            <div className="absolute rounded-2xl inset-0  flex items-center justify-center bg-black opacity-50 hover:opacity-60 transition-opacity duration-300"></div>
-          </Link>
-        ))}
-      </div>
+              {' '}
+              {
+                <Image
+                  width={200}
+                  height={200}
+                  src="/"
+                  alt={category.name}
+                  className="w-[100%] h-[100%] object-center object-cover rounded-2xl"
+                />
+              }
+              <p
+                style={{ color: 'white' }}
+                className="text-sm leading-none sm:leading-1 sm:text-[1rem]  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center absolute z-30 p-2  shadow-xl "
+              >
+                {category.name}
+              </p>
+              <div className="absolute rounded-2xl inset-0  flex items-center justify-center bg-black opacity-50 hover:opacity-60 transition-opacity duration-300"></div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
