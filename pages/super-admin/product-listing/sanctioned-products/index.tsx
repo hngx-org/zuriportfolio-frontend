e510a6e5f266e6b279924ab9080b942453952d55
segunojo1 <olusegunkd2017@gmail.com>
2023-10-14 08:34:34 +0100

    test

--- pages/super-admin/product-listing/sanctioned-products/index.tsx
+++ pages/super-admin/product-listing/sanctioned-products/index.tsx
@@ -26,10 +26,12 @@ const SanctionedProducts = () => {
 
   const handleSearch = (searchText: string) => {
     const filteredProduct: any = data?.data?.filter(
-      (product: any) => product?.product_name?.toLowerCase()?.includes(searchText.toLowerCase()),
+      (product: any) =>
+        product?.product_name?.toLowerCase()?.includes(searchText.toLowerCase()) &&
+        product.product_status.toLowerCase().includes('sanctioned'),
     );
     setSearchVal(searchText);
-    setFilteredProducts(filteredProducts);
+    setFilteredProducts(filteredProduct);
   };
 
   const route = useRouter();
