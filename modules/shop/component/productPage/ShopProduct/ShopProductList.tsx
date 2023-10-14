0ea93da854d96198634ff33f9e3b05f1f07ffbdd
Miracle Okeke <mokeke185@gmail.com>
2023-10-14 20:39:29 +0100

    Merged changes from dev into HAR-1/Feat/Zuri-Shop-Landing-Page

--- modules/shop/component/productPage/ShopProduct/ShopProductList.tsx
+++ modules/shop/component/productPage/ShopProduct/ShopProductList.tsx
@@ -21,7 +21,7 @@ const ShopProductList: React.FC<ShopProductListProps> = ({ products, searchQuery
   });
 
   return (
-    <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-10 gap-2">
+    <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 md:gap-10 gap-2">
       {filteredProducts.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
