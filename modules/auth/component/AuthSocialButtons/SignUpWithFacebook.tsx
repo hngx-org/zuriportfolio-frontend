edd10fc7001d5e595d79f45f47224eee0f9e4e67
Ezeh20 <ranter1570@gmail.com>
2023-10-14 15:53:21 +0100

    staging

--- modules/auth/component/AuthSocialButtons/SignUpWithFacebook.tsx
+++ modules/auth/component/AuthSocialButtons/SignUpWithFacebook.tsx
@@ -5,7 +5,10 @@ import facebookLogo from '../../../../public/assets/images/logo/facebook-logo.sv
 
 const SignUpWithFacebook = () => {
   return (
-    <SignUpWithSocialsButton leftIcon={<Image src={facebookLogo} alt="Google logo" className="w-5 h-5" />}>
+    <SignUpWithSocialsButton
+      href="https://auth.akuya.tech/api/auth/facebook"
+      leftIcon={<Image src={facebookLogo} alt="Google logo" className="w-5 h-5" />}
+    >
       Continue with Facebook
     </SignUpWithSocialsButton>
   );
