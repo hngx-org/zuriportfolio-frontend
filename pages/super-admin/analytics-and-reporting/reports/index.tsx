a775938e6e4f0ae4e533d9963ab8e1d3b21a510d
Layconnn <olamilekanjinadu21@gmail.com>
2023-10-14 17:42:40 -0700

    adding export report function

--- pages/super-admin/analytics-and-reporting/reports/index.tsx
+++ pages/super-admin/analytics-and-reporting/reports/index.tsx
@@ -15,6 +15,7 @@ import axios from 'axios';
 const AnalyticsAndReport: React.FC = () => {
   const [loading, setLoading] = useState<Boolean>(true);
   const [reportModalOpen, setReportModalOpen] = useState<Boolean>(false);
+  const [selectedFileFormat, setSelectedFileFormat] = useState<string>('');
   const [isLoading, setIsLoading] = useState<Boolean>(false);
   const [getReport, setGetReport] = useState<Boolean>(false);
   const [reportClicked, setReportClicked] = useState<Boolean>(false);
@@ -67,6 +68,44 @@ const AnalyticsAndReport: React.FC = () => {
 
   const handleDateRangeChange = (dateRange: DateObject[]) => {
     setSelectedDateRange(dateRange);
+  };
+
+  function saveFile(data: BlobPart, filename: string) {
+    const blob = new Blob([data], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+  const handleExport = () => {
+    console.log('handleExport called');
+    if (selectedDateRange.length === 2) {
+      const startDate = selectedDateRange[0].format('YYYY-MM-DD');
+      const endDate = selectedDateRange[1].format('YYYY-MM-DD');
+      setGetReport(true);
+
+      const apiUrl = `https://team-mirage-super-amind2.onrender.com/api/superadmin/analytics/export_report/?file_format=${selectedFileFormat}&start_date=${startDate}&end_date=${endDate}&page=1&page_size=10`;
+
+      axios
+        .get(apiUrl, { responseType: 'arraybuffer' })
+        .then((response) => {
+          const filename = `Data.${selectedFileFormat}`;
+          console.log(response);
+          saveFile(response.data, filename);
+          setGetReport(false);
+        })
+        .catch((error) => {
+          setGetReport(false);
+          console.error('Error exporting report:', error);
+        });
+    }
+  };
+
+  const handleFileFormatSelect = (format: string) => {
+    setSelectedFileFormat(format);
   };
 
   return (
@@ -139,20 +178,48 @@ const AnalyticsAndReport: React.FC = () => {
                       Choose Format
                     </p>
                     <div className="flex justify-evenly">
-                      <div className="flex justify-center items-center max-w-[9.59425rem] w-full rounded-[0.95944rem] text-[#8D9290] py-[1.2rem] font-manropeL hover:text-[#009254] focus:bg-[#E6F5EA] hover:border hover:border-[#33A467] border-gray-300 shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] hover:bg-[#E6F5EA] text-center leading-[2.87825rem] tracking-[0.00288rem] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] max-[768px]:leading-[1.278rem] focus:text-[#009254] focus:border focus:border-[#33A467] cursor-pointer">
+                      <div
+                        className={`${
+                          selectedFileFormat === 'excel'
+                            ? 'border border-[#33A467] text-[#009254] bg-[#E6F5EA] flex justify-center items-center max-w-[9.59425rem] w-full rounded-[0.95944rem] py-[1.2rem] font-manropeL shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] hover:bg-[#E6F5EA] text-center leading-[2.87825rem] tracking-[0.00288rem] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] max-[768px]:leading-[1.278rem] cursor-pointer'
+                            : 'flex justify-center items-center max-w-[9.59425rem] w-full rounded-[0.95944rem] text-[#8D9290] py-[1.2rem] font-manropeL hover:text-[#009254] focus:bg-[#E6F5EA] hover:border hover:border-[#33A467] border-gray-300 shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] hover:bg-[#E6F5EA] text-center leading-[2.87825rem] tracking-[0.00288rem] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] max-[768px]:leading-[1.278rem] focus:text-[#009254] focus:border focus:border-[#33A467] cursor-pointer'
+                        } cursor-pointer`}
+                        onClick={() => handleFileFormatSelect('excel')}
+                      >
                         Excel
                       </div>
-                      <div className="flex justify-center items-center max-w-[9.59425rem] w-full font-manropeL rounded-[0.95944rem] py-[1.2rem] focus:bg-[#E6F5EA] shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] text-center leading-[2.87825rem] tracking-[0.00288rem] text-[#8D9290] border-gray-300 hover:bg-[#E6F5EA] hover:text-[#009254] hover:border hover:border-[#33A467] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] focus:text-[#009254] focus:border focus:border-[#33A467] max-[768px]:leading-[1.278rem] cursor-pointer">
+                      <div
+                        className={`${
+                          selectedFileFormat === 'pdf'
+                            ? 'border border-[#33A467] text-[#009254] bg-[#E6F5EA] flex justify-center items-center max-w-[9.59425rem] w-full rounded-[0.95944rem] py-[1.2rem] font-manropeL shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] hover:bg-[#E6F5EA] text-center leading-[2.87825rem] tracking-[0.00288rem] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] max-[768px]:leading-[1.278rem] cursor-pointer'
+                            : 'flex justify-center items-center max-w-[9.59425rem] w-full rounded-[0.95944rem] text-[#8D9290] py-[1.2rem] font-manropeL hover:text-[#009254] focus:bg-[#E6F5EA] hover:border hover:border-[#33A467] border-gray-300 shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] hover:bg-[#E6F5EA] text-center leading-[2.87825rem] tracking-[0.00288rem] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] max-[768px]:leading-[1.278rem] focus:text-[#009254] focus:border focus:border-[#33A467] cursor-pointer'
+                        } cursor-pointer`}
+                        onClick={() => handleFileFormatSelect('pdf')}
+                      >
                         PDF
                       </div>
-                      <div className="flex justify-center items-center max-w-[9.59425rem] w-full font-manropeL rounded-[0.95944rem] py-[1.2rem] text-[#8D9290] border-gray-300 shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] text-center leading-[2.87825rem] tracking-[0.00288rem] hover:text-[#009254] hover:bg-[#E6F5EA] focus:bg-[#E6F5EA] hover:border hover:border-[#33A467] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] max-[768px]:leading-[1.278rem] focus:text-[#009254] focus:border focus:border-[#33A467]  cursor-pointer">
+                      <div
+                        className={`${
+                          selectedFileFormat === 'csv'
+                            ? 'border border-[#33A467] text-[#009254] bg-[#E6F5EA] flex justify-center items-center max-w-[9.59425rem] w-full rounded-[0.95944rem] py-[1.2rem] font-manropeL shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] hover:bg-[#E6F5EA] text-center leading-[2.87825rem] tracking-[0.00288rem] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] max-[768px]:leading-[1.278rem] cursor-pointer'
+                            : 'flex justify-center items-center max-w-[9.59425rem] w-full rounded-[0.95944rem] text-[#8D9290] py-[1.2rem] font-manropeL hover:text-[#009254] focus:bg-[#E6F5EA] hover:border hover:border-[#33A467] border-gray-300 shadow-[0px_1.91885px_3.83771px_0px_rgba(16,24,40,0.05)] text-[1.9188rem] hover:bg-[#E6F5EA] text-center leading-[2.87825rem] tracking-[0.00288rem] max-[768px]:text-[0.84725rem] max-[768px]:w-[4.23625rem] max-[768px]:py-[0.74rem] max-[768px]:leading-[1.278rem] focus:text-[#009254] focus:border focus:border-[#33A467] cursor-pointer'
+                        } cursor-pointer`}
+                        onClick={() => handleFileFormatSelect('csv')}
+                      >
                         CSV
                       </div>
                     </div>
                   </div>
                   <div className="flex justify-evenly">
-                    <div className="flex justify-center items-center bg-[#009254] rounded-[1.91888rem] max-w-[17.50956rem] w-full h-[5.75656rem] font-manropeL text-[#FFF] text-[1.5rem] font-extrabold tracking-[0.00088rem] leading-[1.375rem] max-[768px]:text-[0.875rem] max-[768px]:w-[7.73119rem] max-[768px]:h-[2.5rem]">
-                      Confirm
+                    <div
+                      className="flex justify-center items-center bg-[#009254] rounded-[1.91888rem] max-w-[17.50956rem] w-full h-[5.75656rem] font-manropeL text-[#FFF] text-[1.5rem] font-extrabold tracking-[0.00088rem] leading-[1.375rem] max-[768px]:text-[0.875rem] max-[768px]:w-[7.73119rem] max-[768px]:h-[2.5rem]"
+                      onClick={handleExport}
+                    >
+                      {getReport ? (
+                        <div className="w-6 h-6 border-t-2 border-[#fff] border-solid rounded-full animate-spin"></div>
+                      ) : (
+                        'Confirm'
+                      )}
                     </div>
                     <div
                       className="flex justify-center items-center bg-[#F4FBF6] border-[1.919px] border-[#009254] rounded-[1.91888rem] max-w-[17.50956rem] w-full h-[5.75656rem] font-manropeL text-[#009254] text-[1.5rem] font-extrabold tracking-[0.00088rem] leading-[1.375rem] max-[768px]:text-[0.875rem] max-[768px]:w-[7.73119rem] max-[768px]:h-[2.5rem] cursor-pointer"
