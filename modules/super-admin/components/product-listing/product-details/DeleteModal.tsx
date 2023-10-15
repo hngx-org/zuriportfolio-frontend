550c4c8eb8cd4c99fb2f06eb6833792621214c35
nwochachioma <nwochachioma1998@gmail.com>
2023-10-15 23:07:54 +0100

    fix: super admin fix

--- modules/super-admin/components/product-listing/product-details/DeleteModal.tsx
+++ modules/super-admin/components/product-listing/product-details/DeleteModal.tsx
@@ -2,7 +2,7 @@ import Button from '@ui/Button';
 import Modal from '@ui/Modal';
 import { CloseCircle } from 'iconsax-react';
 import { useState } from 'react';
-import { useDeleteProd } from '../../../../../http';
+import { useDeleteProd, useDeleteShop } from '../../../../../http';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
 import { handleBack } from '.';
@@ -14,6 +14,7 @@ const DeleteModal = ({
   setReasons,
   id,
   data,
+  type,
 }: {
   isOpen: boolean;
   closeModal: () => void;
@@ -21,6 +22,7 @@ const DeleteModal = ({
   setReasons: React.Dispatch<React.SetStateAction<Map<string, string>>>;
   id: string;
   data: Record<string, any>;
+  type: 'product' | 'vendor';
 }) => {
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const reasonsList = ['Policy violations', 'Offensive words', 'Just feel like it', 'Other'];
@@ -35,9 +37,10 @@ const DeleteModal = ({
   };
   const reasonsKeysArray = Array.from(reasons.keys());
   const { deleteSanction, isLoading: isDeleting } = useDeleteProd();
+  const { deleteShop, isLoading: isDeletingShop } = useDeleteShop();
   const route = useRouter();
 
-  const handleDelete = () => {
+  const handleDeleteProd = () => {
     deleteSanction(id, {
       onSuccess: (response) => {
         if (response.response.status < 300) {
@@ -59,10 +62,19 @@ const DeleteModal = ({
     <>
       <Modal isOpen={isOpen} closeModal={closeModal} closeOnOverlayClick isCloseIconPresent={false} size="xl">
         <div className="p-5 flex flex-col gap-8">
-          <h2 className="font-manropeB text-[28px] lg:text-[36px]">Delete Product</h2>
+          <h2 className="font-manropeB text-[28px] lg:text-[36px] capitalize">Delete {type}</h2>
           <p className="font-manrope leading-[28px] text-[22px]">
-            <span className="font-bold">{data?.product_name}</span> will be deleted as a vendor from Zuri Marketplace
-            and all their products as well. They will get a notification email.
+            {type === 'product' ? (
+              <span>
+                <span className="font-bold">{data?.product_name}</span> will be deleted as a vendor from Zuri
+                Marketplace and all their products as well. They will get a notification email.
+              </span>
+            ) : (
+              <span>
+                <span className="font-bold">{data?.merchant_name}</span> will be deleted as a vendor from Zuri
+                Marketplace and all their products as well. They will get a notification email.
+              </span>
+            )}
           </p>
           <div>
             <p className="text-[20px] lg:text-[22px] mb-4">Reason for deleting:</p>
@@ -130,18 +142,22 @@ const DeleteModal = ({
         </div>
         <div>
           <p className="text-center font-manropeB text-[24px] w-[331px] mx-auto leading-[32px]">
-            Are you sure you want to delete this product?
+            Are you sure you want to delete this {type}?
           </p>
-          <p className="mt-2 text-[16px] text-center">Product will be permanently deleted from list.</p>
+          <p className="mt-2 text-[16px] text-center">
+            <span className="capitalize">{type}</span> will be permanently deleted from list.
+          </p>
           <div className="flex flex-col gap-3 mt-8">
             <Button
               intent={'error'}
               spinnerColor="#000"
-              className="w-full rounded-[16px]"
-              isLoading={isDeleting}
-              onClick={handleDelete}
+              className="w-full rounded-[16px] capitalize"
+              isLoading={isDeleting || isDeletingShop}
+              onClick={() => {
+                type === 'product' ? handleDeleteProd() : handleDeleteShop();
+              }}
             >
-              Delete Product
+              Delete {type}
             </Button>
             <Button
               intent={'secondary'}
