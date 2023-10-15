a3bb58d00555858e29b11e705d1c0ccdf2528874
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-10-15 17:35:06 +0100

    feat: updated next.config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com', 'staging.zuri.team', 'zuri.team', 'avatars.githubusercontent.com'],
+    domains: ['res.cloudinary.com', 'staging.zuri.team', 'zuri.team', 'avatars.githubusercontent.com', 'ugc.futurelearn.com', 'themeforest.img.customer.envatousercontent.com'],
   },
 };
 
