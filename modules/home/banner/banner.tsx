3d0aff54f431cc5cab2bab1cf1ee2721383ac8de
Shalom Taiwo <ifeshalomt@gmail.com>
2023-10-16 11:08:20 +0200

    jumbotron top

--- modules/home/banner/banner.tsx
+++ modules/home/banner/banner.tsx
@@ -7,12 +7,12 @@ const Banner = () => {
   return (
     <div className="h-[56px] bg-blue-250 w-full flex justify-center items-center">
       <div className="flex justify-center items-center p-[10px]">
-        <Button className=" hidden md:h-[30px] md:flex md:bg-green-200 md:rounded-[10px] md:hover:bg-green-200 md:mr-2 ">
+        <div className=" hidden text-white-100 p-4 md:h-[30px] md:flex md:justify-center md:items-center md:bg-green-200 md:rounded-[10px] md:hover:bg-green-200 md:mr-2 ">
           FEATURED
-        </Button>
+        </div>
         <span className="text-[14px] font-manropeL text-center">Create a shop for all your Digital Products!</span>
         <Link href="/marketplace">
-          <ArrowRight className="ml-2 mt-[1px]" color="#1A1C1F" size={16} />
+          <ArrowRight className="ml-2 -mt-[1px]" color="#1A1C1F" size={16} />
         </Link>
       </div>
     </div>
--- modules/home/jumbotron/jumbotron.tsx
+++ modules/home/jumbotron/jumbotron.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import JumboBg from '../../../public/assets/home/jumbo_background.png';
+import JumboIconOne from '../../../public/assets/home/jumbo-circle-one.png';
+import JumboIconTwo from '../../../public/assets/home/jumbo-circle-two.png';
+import JumboIconThree from '../../../public/assets/home/jumbo-circle-three.png';
 import Button from '@ui/Button';
+import Image from 'next/image';
 
 const Jumbotron = () => {
   return (
     <div className="w-full md:w-[80%]">
       <div className="bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${JumboBg.src})` }}>
-        <div className="pt-[60px] px-2 md:pt-[100px] text-center flex flex-col justify-center items-center">
-          <div>
+        <div className="space-y-2 bg-gradient-to-b from-[#ffffff83] to-white-100 px-2 md:pt-[100px] text-center flex flex-col justify-center items-center">
+          <div className="relative pt-[50px] space-y-2">
+            <div className="absolute -right-20 -top-[20px] hidden md:block">
+              <Image src={JumboIconOne.src} alt="Jumbo Icon One" width={84} height={84} />
+            </div>
             <h2 className="text-[#1A1C1B] text-[36px] md:text-[48px] font-manropeEB leading-[44px]">
               Explore. Showcase. Sell. Connect.
             </h2>
@@ -15,18 +22,22 @@ const Jumbotron = () => {
               A link in bio, <span className="text-[#009254] underline decoration-1">But</span> for Talents!
             </h3>
           </div>
-          <div className="mt-[10px] text-center text-[#1A1C1B] text-[16px] font-manropeL">
+          <div className=" relative text-center text-[#1A1C1B] text-[16px] font-manropeL">
+            <div className="absolute -left-[100px] -top-[30px] hidden md:block">
+              <Image src={JumboIconThree.src} alt="Jumbo Icon Three" width={84} height={84} className="-ml-[45px]" />
+              <Image src={JumboIconTwo.src} alt="Jumbo Icon Two" width={84} height={84} className="-mt-[84px]" />
+            </div>
             <p className="sm:flex sm:flex-col">
               <span>Join Talents using Zuri Portfolio to increase audience for things digital</span>{' '}
               <span> - Your Portfolio. Your shop!</span>
             </p>
           </div>
-          <div className="mt-[20px] flex justify-center items-center">
+          <div className="pt-[15px] flex justify-center items-center">
             <Button href="/auth/signup" className="text-[16px] rounded-[8px]">
               Get Started
             </Button>
             <Button
-              href="/auth/signup"
+              href="/explore"
               className="text-[16px] bg-white-100 hover:bg-white-100 text-green-500 underline underline-offset-[12px]"
             >
               Portfolio Explore
