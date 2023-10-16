6bcef7898b832f519cc3ba7cddf3064af58e78e3
homoakin619 <homohaykins619@gmail.com>
2023-10-16 14:11:34 +0100

    fixed dicount price display on cart page

--- http/checkout.ts
+++ http/checkout.ts
@@ -1,14 +1,10 @@
 import { CartItemProps } from '../@types';
 import $http from './axios';
 
-// || 'https://zuri-cart-checkout.onrender.com/api/checkout';
-// || 'http://localhost:3000';
-// || 'https://coral-app-8bk8j.ondigitalocean.app/api/recently-viewed';
-
-
-export const CART_ENDPOINT = process.env.NEXT_PUBLIC_CART_API_URL 
-export const STAGING_URL = process.env.NEXT_PUBLIC_APP_STAGING_URL 
-export const RECENTLY_VIEWED_ENDPOINT = process.env.NEXT_PUBLIC_RECENTLY_VIEWED_ENDPOINT 
+export const CART_ENDPOINT = process.env.NEXT_PUBLIC_CART_API_URL || 'https://zuri-cart-checkout.onrender.com/api/checkout';
+export const STAGING_URL = process.env.NEXT_PUBLIC_APP_STAGING_URL || 'https://staging.zuri.team';
+export const RECENTLY_VIEWED_ENDPOINT =
+  process.env.NEXT_PUBLIC_RECENTLY_VIEWED_ENDPOINT || 'https://coral-app-8bk8j.ondigitalocean.app/api/recently-viewed';
 
 const guestToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI3NTkwNzY4LTZhZjUtNGFiMS1hNGEwLWZiNmQ5NzM4Y2JmMCIsImlhdCI6MTY5NzQ0NjY1NH0.BGIinA0uWtPFlf0tu2J_i_oCLOwWCKSVA5kwRX2oMiQ"
 
