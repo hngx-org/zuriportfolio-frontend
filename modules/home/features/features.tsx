7a464e54970b300d7add20917e097547c2e42465
Shalom Taiwo <ifeshalomt@gmail.com>
2023-10-16 15:15:05 +0200

    features

---
+++ modules/home/features/features.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+  data: object;
+}
+const Features = ({ data }: Props) => {
+  return (
+    <div className="flex justify-center items-center pb-10">
+      <div></div>
+    </div>
+  );
+};
+
+export default Features;
---
+++ modules/home/sections/second/sectionTwo.tsx
@@ -0,0 +1,14 @@
+import Features from '@modules/home/features/features';
+import React from 'react';
+
+const data: never[] = [];
+
+const SectionTwo = () => {
+  return (
+    <div className="flex justify-center items-center pb-10">
+      <Features data={data} />
+    </div>
+  );
+};
+
+export default SectionTwo;
