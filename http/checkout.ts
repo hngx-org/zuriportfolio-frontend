e80c2a02271b2099353428415d23adaefc59461e
homoakin619 <homohaykins619@gmail.com>
2023-10-16 11:55:31 +0100

    tested guest user purchase flow and added fixes

--- http/checkout.ts
+++ http/checkout.ts
@@ -2,13 +2,10 @@ import { CartItemProps } from '../@types';
 import $http from './axios';
 
 const CART_ENDPOINT = process.env.NEXT_PUBLIC_CART_API_URL || 'https://zuri-cart-checkout.onrender.com/api/checkout';
-const STAGING_URL = process.env.NEXT_PUBLIC_APP_STAGING_URL || 'https://zuriportfolio-frontend-pw1h.vercel.app';
+const STAGING_URL = process.env.NEXT_PUBLIC_APP_STAGING_URL || 'http://localhost:3000';
 const RECENTLY_VIEWED_ENDPOINT =
   process.env.NEXT_PUBLIC_RECENTLY_VIEWED_ENDPOINT || 'https://coral-app-8bk8j.ondigitalocean.app/api/recently-viewed';
-// Guest: 
-// akuyaekorot+hng19@gmail.com
-// b7590768-6af5-4ab1-a4a0-fb6d9738cbf0
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI3NTkwNzY4LTZhZjUtNGFiMS1hNGEwLWZiNmQ5NzM4Y2JmMCIsImlhdCI6MTY5NzQ0NjY1NH0.BGIinA0uWtPFlf0tu2J_i_oCLOwWCKSVA5kwRX2oMiQ
+
 const guestToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI3NTkwNzY4LTZhZjUtNGFiMS1hNGEwLWZiNmQ5NzM4Y2JmMCIsImlhdCI6MTY5NzQ0NjY1NH0.BGIinA0uWtPFlf0tu2J_i_oCLOwWCKSVA5kwRX2oMiQ"
 
 export const addToCart = async (cartItems: string[],token: string) => {
@@ -87,24 +84,15 @@ export const getCartSummary = async (token:string) => {
     }
 };
 
-// sicoj92691@elixirsd.com
-// myPassword2021
-
-// {
-//     "email": "akuyaekorot+hng6@gmail.com",
-//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImE5NDE3MzIxLTk0OTQtNDQwNC05MjgyLWI3YjgyZDI4ZWQwZiIsImlhdCI6MTY5NzQ0MjY0N30.iWAHaXHssvsuG6Gb6MBOOZjtA8zvUhOfBwb9EqQamv0",
-//     "password": "password_akuya"
-// }
 
 export const getGuestCartSummary = async (products:any[]) => {
     
     try {
-    const apiUrl = "https://zuri-cart-checkout.onrender.com/api/checkout/api/carts/guest-cart-summary";
+    const apiUrl = `${CART_ENDPOINT}/api/carts/guest-cart-summary`;
 
     const response = await $http.post(apiUrl,{product_ids: products}, {
         headers: {
           'Content-Type': 'application/json',
-          // 'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImE3YjRiOThiLWFlMzMtNGQ0Yy1hNmUzLTQ4YzY5MGQ5NDUyMyIsImlhdCI6MTY5NzM3MDcxNH0.dBJSQ3zzSXiw55fqjLlWE6cmk1xmtpQxSSne9cZbOAg`
         },
       },
     );
@@ -120,7 +108,7 @@ export const makePayment = async (selectedPaymentMethod: string, token: string) 
     try {
       const apiUrl = `${CART_ENDPOINT}/api/orders`;
       const data = {
-        redirect_url: `http://localhost:3000/marketplace/success`,
+        redirect_url: `${STAGING_URL}/marketplace/success`,
         payment_method: selectedPaymentMethod,
       };
 
