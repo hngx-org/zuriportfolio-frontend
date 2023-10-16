3e3e07a8cdc373af210b3b1c5709765fdc8ea912
BaZz <bazuaye50@gmail.com>
2023-10-16 01:16:18 +0100

    Merge pull request #916 from hngx-org/fix-staging-missing-files
    verfication

--- modules/auth/verificationComplete.tsx
+++ modules/auth/verificationComplete.tsx
@@ -8,13 +8,16 @@ import { notify } from '@ui/Toast';
 import isAuthenticated from '../../helpers/isAuthenticated';
 import { useAuth } from '../../context/AuthContext';
 import persistedToken from '../../helpers/persistedToken';
+import ChangeEmailAddress from './changeEmailAddress';
+import ResendVerification from './resendVerification';
 
 function VerificationComplete() {
   const router = useRouter();
   const { token } = router.query;
   const { handleAuth } = useAuth();
+  const [isError, setIsError] = useState(false);
 
-  console.log(token);
+  // console.log(token);
 
   let tokenFromLocalStorage: string = '';
 
@@ -24,15 +27,12 @@ function VerificationComplete() {
 
   const decodedToken = persistedToken(tokenFromLocalStorage as string);
 
-  console.log(decodedToken);
+  // console.log(decodedToken);
 
   const { mutate, isLoading, isSuccess } = useAuthMutation(verifyUser, {
     onSuccess: (response) => {
-      console.log(response);
-
-      console.log(isSuccess);
-
       if (response.status === 200) {
+        setIsError(false);
         handleAuth(response.data);
         localStorage.setItem('zpt', response?.data?.token);
 
@@ -45,19 +45,35 @@ function VerificationComplete() {
         return;
       }
 
+      if (response.status !== 200) {
+        setIsError(true);
+        notify({
+          message: response.data.message,
+          type: 'error',
+        });
+        return;
+      }
+
       notify({
         message: 'Verification Unsuccessful!',
         type: 'error',
       });
     },
     onError: ({ response }: any) => {
+      if (!isSuccess) {
+        const resend = 'Invalid token / Expired token';
+
+        notify({ message: resend, type: 'error' });
+        setIsError(true);
+        return;
+      }
       console.log('verificaion error z', response);
 
       if (response.data.message === 'timeout of 30000ms exceeded') {
         const timeoutErrorMessage =
           'Oops! The request timed out. Please try again later. If the problem persists, please contact support.';
 
-        console.log(response.data.message);
+        console.log(response);
 
         notify({
           message: timeoutErrorMessage,
@@ -78,6 +94,8 @@ function VerificationComplete() {
   const verifyQuery = async () => {
     if (token) {
       mutate({ token: token as any });
+    } else {
+      setIsError(true);
     }
   };
 
@@ -92,7 +110,7 @@ function VerificationComplete() {
 
   return (
     <VerificationLayout>
-      {isSuccess && (
+      {isSuccess && !isError && (
         <>
           <Image
             className="w-[218px] h-[159px] xl:w-[218px] xl:h-[218px] mx-auto mt-16 md:mt-16 lg:"
@@ -109,6 +127,8 @@ function VerificationComplete() {
           </div>
         </>
       )}
+
+      {!isLoading && isError && <ResendVerification />}
 
       {isLoading && (
         <div className=" sm:bg-brand-green-ttr px-4 max-w-[712px] sm:px-[40px] md:px-[58px] lg:px-[120px] py-5 sm:border sm:border-brand-disabled rounded-[32px] z-10">
