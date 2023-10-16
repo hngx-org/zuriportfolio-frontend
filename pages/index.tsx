c4f9826ae0e49ad1f76d63fd92d5bc0df01b1986
orionEsu <orionesu.dev@gmail.com>
2023-10-16 11:31:13 +0100

    Merge branch 'dev' of https://github.com/hngx-org/zuriportfolio-frontend into MAR-43/Fix/Marketplace-fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,16 @@ import Link from 'next/link';
 import MainLayout from '../components/Layout/MainLayout';
 
 import ProductDetailsDescription from '../modules/marketplace/productDetailsDescription';
+import Banner from '@modules/home/banner/banner';
+import SectionOne from '@modules/home/sections/first/sectionOne';
 
 function Home() {
-  return <MainLayout activePage="home" showDashboardSidebar showTopbar></MainLayout>;
+  return (
+    <MainLayout activePage="home" showDashboardSidebar={false} showTopbar includeMarginTop={false}>
+      <Banner />
+      <SectionOne />
+    </MainLayout>
+  );
 }
 
 export default Home;
