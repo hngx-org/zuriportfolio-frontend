507f25349d586029a9a5ed11624a1f71be435273
Joshua Edo <joshua.edo01@gmail.com>
2023-10-16 16:09:50 +0000

    feat: add tooltip feature

--- modules/dashboard/component/dashboard/chart.tsx
+++ modules/dashboard/component/dashboard/chart.tsx
@@ -1,7 +1,8 @@
 import Loader from '@ui/Loader';
 import React from 'react';
-import { BarChart, Bar, XAxis, CartesianGrid, LineChart, Line, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, CartesianGrid, LineChart, Tooltip, Line, ResponsiveContainer } from 'recharts';
 import { ChartProps } from '../../../../@types/index';
+import { sevenDays, twelveMonths, twentyFourHours, thirtyDays } from '../../../../db/dashboard';
 
 const Chart: React.FC<ChartProps> = ({ isBarChart, data, isFetching, isFetched }) => {
   return (
@@ -17,14 +18,22 @@ const Chart: React.FC<ChartProps> = ({ isBarChart, data, isFetching, isFetched }
             <BarChart width={800} height={250} data={data} margin={chartMargins}>
               <CartesianGrid vertical={false} strokeDasharray="1 0" />
               <XAxis dataKey="timeline" />
-              <Bar barSize={30} dataKey="income" fill="#CBEAD4" />
+              <Tooltip content={<CustomTooltip />} />
+              <Bar barSize={30} dataKey="income" fill="#CBEAD4" className="cursor-pointer" />
             </BarChart>
           ) : (
             <LineChart width={800} height={250} data={data} margin={chartMargins}>
               <CartesianGrid vertical={false} strokeDasharray="1 0" />
               <XAxis dataKey="timeline" />
-              <Line dot={false} type="monotone" dataKey="income1" stroke="#F1D5BA" strokeWidth={2.5} />
-              <Line dot={false} type="monotone" dataKey="income2" stroke="#E1BD90" strokeWidth={2.5} />
+              <Tooltip content={<CustomTooltip />} />
+              <Line
+                dot={true}
+                type="monotone"
+                dataKey="income"
+                stroke="#E1BD90"
+                strokeWidth={2.5}
+                className="cursor-pointer"
+              />
             </LineChart>
           )}
         </ResponsiveContainer>
@@ -39,5 +48,36 @@ const chartMargins = {
   left: 20,
   bottom: 5,
 };
+
+const CustomTooltip = ({ active, payload, label }: any) => {
+  const tooltipMessage = getTooltipMessage(label, payload);
+  if (active && payload && payload.length) {
+    return (
+      <div className="bg-zinc-100 opacity-100 shadow rounded-md p-3 text-sm md:text-base">
+        <p className="">{tooltipMessage}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+function getTooltipMessage(label: any, payload: any[]) {
+  let message = `Your total revenue `;
+
+  if (twelveMonths.includes(label)) {
+    message += `in ${label} was $${payload[0]?.value}`;
+  } else if (sevenDays.includes(label)) {
+    message += `on ${label} was $${payload[0]?.value}`;
+  } else if (thirtyDays.includes(label)) {
+    message += `on the ${label} was $${payload[0]?.value}`;
+  } else if (twentyFourHours.includes(label)) {
+    message += `at ${label} was $${payload[0]?.value}`;
+  } else {
+    message += `for ${label} was $${payload[0]?.value}`;
+  }
+
+  return message;
+}
 
 export default Chart;
--- modules/dashboard/component/dashboard/charts.tsx
+++ modules/dashboard/component/dashboard/charts.tsx
@@ -77,10 +77,9 @@ export const MetricChart = ({ title, src, isBarChart }: MetricChartProps) => {
   console.log(mockSalesReportData);
 
   const mockStoreTrafficData = Array.from({ length: numBarsAsNumber }, (_, i) => {
-    const income1: number = Math.floor(Math.random() * 1001) + 500;
-    const income2: number = Math.floor(Math.random() * 1001) + 500;
+    const income: number = Math.floor(Math.random() * 1001) + 500;
     const timeline: string = chartData[i % chartData.length];
-    return { timeline, income1, income2 };
+    return { timeline, income };
   });
   console.log(mockStoreTrafficData);
 
