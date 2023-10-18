f3fcc25e7ce2053b216de9a5434004ef07b11731
ASANIYAN <ayomidepaulasaniyan@gmail.com>
2023-10-18 04:29:15 +0100

    fixed bugs and styling issues for most sections

--- modules/portfolio/component/all-projects-modal.tsx
+++ modules/portfolio/component/all-projects-modal.tsx
@@ -1,53 +1,101 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import projectPlaceholder from '../../../public/assets/images/portfolio/project.png';
 import Link from 'next/link';
-import { ProjectModalProps } from '../../../@types';
+import { Data, allRouteOptions } from './modals/project-section-modal';
+import Button from '@ui/Button';
+import { Add } from 'iconsax-react';
 
-const AllProjectsModal = () => {
-  const url = '';
-  const description =
-    'Implemented A/B testing for website redesign, leading to a 40% user retention and a 25% increase in conversion rates, ultimately resulting in a 20% boost in overall revenue.';
-  const title = 'Byte Financial App';
-  const tags = ['UI/UX', 'figma', 'miro', 'Adobe XD', 'miro', 'sketch', 'Adobe XD'];
+const endpoint = 'https://hng6-r5y3.onrender.com';
+const AllProjectsModal = ({
+  onEdit,
+  projects,
+  handleSetRoute,
+}: {
+  projects: any[];
+  onEdit: (data: Data) => void;
+  handleSetRoute: (data: allRouteOptions) => void;
+}) => {
+  const handleEdit = (data: Data) => {
+    onEdit(data);
+  };
 
   return (
-    <>
-      <section className="flex flex-wrap gap-10 mt-10">
-        <section className="w-full min-[920px]:w-[300px] h-[300px]">
-          <Image src={projectPlaceholder} className="w-full h-full" alt="Project sample image" />
-        </section>
-        <section className="min-[920px]:flex-1 font-manropeL">
-          <h2 className="font-manropeEB text-2xl sm:text-3xl md:text-4xl">{title}</h2>
+    <section className="p-5">
+      <section className="h-[400px] overflow-y-auto">
+        {projects.length > 0 &&
+          projects.map((project: Data) => {
+            const { description, tags, url, title, thumbnail } = project;
+            return (
+              <>
+                <section className="flex flex-wrap gap-10 mt-10">
+                  <section className="w-full min-[920px]:w-[250px] h-[220px]">
+                    <Image src={thumbnail} width={250} height={220} alt="Project sample image" />
+                  </section>
+                  <section className="min-[920px]:flex-1 font-manropeL">
+                    <h2 className="font-manropeL text-2xl sm:text-3xl md:text-4xl">{title}</h2>
 
-          <p className="font-semibold mt-5 text-sm sm:text-base text-white-650 md:text-xl md:leading-[2rem]">
-            {description}
-          </p>
+                    <p className="font-semibold font-manropeL mt-5 text-sm sm:text-base text-white-650 md:text-xl md:leading-[2rem]">
+                      {description}
+                    </p>
 
-          <div className="flex flex-wrap gap-3 mt-5 mb-5 text-sm text-[#444846] capitalize">
-            {tags.map((tag: string) => (
-              <span key={tag} className="border-2 border-[#8D9290] rounded-full px-2 py-1 font-manropeL">
-                {tag}
-              </span>
-            ))}
-          </div>
+                    <div className="flex flex-wrap gap-3 mt-5 mb-5 text-sm text-[#444846] capitalize">
+                      {tags.split(',').length > 0 &&
+                        tags.split(',').map((tag: string) => (
+                          <span key={tag} className="border-2 border-[#8D9290] rounded-full px-2 py-1 font-manropeL">
+                            {tag}
+                          </span>
+                        ))}
+                    </div>
 
-          <Link
-            href={url}
-            target="_blank"
-            rel="noreferrer"
-            className="font-semibold text-[#5B8DEF] text-sm md:text-base mt-5"
+                    <Link
+                      href={url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="font-semibold text-[#5B8DEF] text-sm md:text-base mt-5 font-manropeL"
+                    >
+                      Link to project <span className="ml-1 text-base">&#8599;</span>
+                    </Link>
+                  </section>
+                </section>
+
+                <section className="flex justify-end gap-2.5 mt-5 text-base font-semibold pr-5">
+                  <span
+                    className="text-[#5B8DEF] cursor-pointer font-manropeL"
+                    onClick={() => {
+                      handleEdit(project);
+                      handleSetRoute('add-project');
+                    }}
+                  >
+                    Edit
+                  </span>
+                  <span className="text-[#FF5C5C] cursor-pointer font-manropeL"> Delete </span>
+                </section>
+
+                <div className="bg-[#E1E3E2] w-full h-[1px] mt-5" />
+              </>
+            );
+          })}
+      </section>
+      <section className="mt-8 sm:mt-16 flex justify-between items-center">
+        <section>
+          <p
+            className="text-base font-semibold font-manropeL flex items-center gap-2 text-green-600 cursor-pointer"
+            onClick={() => handleSetRoute('add-project')}
           >
-            Link to project <span className="ml-1 text-base">&#8599;</span>
-          </Link>
+            <Add color="#009254" /> Add new project
+          </p>
+        </section>
+        <section className="w-fit flex items-center gap-4">
+          <Button intent={'secondary'} className="w-full rounded-md sm:w-[6rem]" size={'lg'}>
+            Cancel
+          </Button>
+          <Button className={`w-full rounded-md sm:w-[6rem]`} size={'lg'}>
+            {' '}
+            Save{' '}
+          </Button>
         </section>
       </section>
-
-      <section className="flex justify-end gap-2.5 mt-5 text-base font-semibold pr-5">
-        <span className="text-[#5B8DEF] cursor-pointer"> Edit </span>
-        <span className="text-[#FF5C5C] cursor-pointer"> Delete </span>
-      </section>
-    </>
+    </section>
   );
 };
 
--- modules/portfolio/component/modals/projects.tsx
+++ modules/portfolio/component/modals/projects.tsx
@@ -9,15 +9,17 @@ import axios from 'axios';
 import { notify } from '@ui/Toast';
 import { checkObjectProperties } from '@modules/portfolio/functions/checkObjectProperties';
 import AllProjectsModal from '../all-projects-modal';
+import { Data } from './project-section-modal';
 
 type ProjectSectionProps = {
   onCloseModal: () => void;
   onSaveModal: () => void;
   userId: string;
+  dataToEdit: Data | null;
 };
 
 const endpoint = 'https://hng6-r5y3.onrender.com';
-const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, onSaveModal }) => {
+const ProjectSection: React.FC<ProjectSectionProps> = ({ dataToEdit, onCloseModal, userId, onSaveModal }) => {
   const [title, setTitle] = useState<string>('');
   const [year, setYear] = useState<string>('');
   const [link, setLink] = useState<string>('');
@@ -28,6 +30,7 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
   const [description, setDescription] = useState<string>('');
   const [media, setMedia] = useState<any[]>([]);
   const [files, setFiles] = useState<any[]>([]);
+  const [id, setId] = useState<number>();
   const years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016];
 
   const items = {
@@ -40,6 +43,18 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
     description,
   };
   const { allChecksPassed, failedChecks } = checkObjectProperties(items);
+
+  const updateParentState = (data: Data) => {
+    const { title, year, link, thumbnail, tags, description, media, id } = data;
+    setTitle(title);
+    setYear(year);
+    setLink(link);
+    setThumbnail(thumbnail);
+    setSelectedTags(tags.split(','));
+    setDescription(description);
+    setMedia(media);
+    setId(id);
+  };
 
   const handleDataClear = () => {
     setTitle('');
@@ -142,15 +157,34 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
     }
   };
 
+  const handleEditData = () => {
+    if (dataToEdit !== null) {
+      const { title, year, link, thumbnail, tags, description, media, id, projectsImages } = dataToEdit;
+      setTitle(title);
+      setYear(year);
+      setLink(link);
+      setThumbnail(thumbnail);
+      setSelectedTags(tags.split(','));
+      setDescription(description);
+      setMedia(projectsImages);
+      setId(id);
+    }
+  };
+
+  useEffect(() => {
+    handleEditData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataToEdit]);
+
   return (
-    <>
+    <section className="p-5">
       {/* header */}
       <div className="flex justify-between items-center">
         <p className="text-[1.2rem] sm:text-[1.5rem] font-bold text-[#2E3130] font-manropeL">Projects</p>
         <CloseSquare size="32" color="#009254" variant="Bold" onClick={close} className="cursor-pointer" />
       </div>
-      <hr className="border-2 rounded-lg border-brand-green-primary md:mb-1 mb-10" />
-      <AllProjectsModal />
+      <hr className="border-2 rounded-lg border-brand-green-primary mt-2.5 md:mb-1 mb-10" />
+      {/* <AllProjectsModal onEdit={updateParentState} userId={userId} /> */}
       <div className="w-full flex-col bg-white-100 p-4 py-5 font-manropeL">
         <div className="flex flex-col gap-5 w-full">
           <form className="flex flex-col gap-5 w-full">
@@ -163,7 +197,9 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
                   onChange={(e) => {
                     setTitle(e.target.value);
                   }}
-                  className="border-[#E1E3E2] w-full h-[50px]  rounded-md border-[2px] text-[12px] font-semibold"
+                  className={`${
+                    failedChecks.includes('title') ? 'border-red-205' : 'border-[#E1E3E2]'
+                  } w-full h-[50px]  rounded-md border-[2px] text-[12px] font-semibold`}
                   inputSize={'lg'}
                   value={title}
                 />
@@ -173,7 +209,9 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
                 <select
                   onChange={(e) => setYear(e.target.value)}
                   placeholder="Year"
-                  className="w-full h-[50px] bg-transparent border-2 rounded-md px-4 border-white-300 font-semibold !text-gray-300"
+                  className={`w-full h-[50px] bg-transparent border-2 rounded-md px-4 ${
+                    failedChecks.includes('year') ? 'border-red-205' : 'border-[#E1E3E2]'
+                  } border-white-300 font-semibold !text-gray-300`}
                 >
                   {years.map((year, index) => (
                     <option className="text-gray-300 bg-transparent" key={index} value={year}>
@@ -206,7 +244,11 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
               <div className="flex-[7] w-full md:w-[50%]">
                 <p className="font-semibold text-gray-200 pb-2">Link to project</p>
                 <div className="flex">
-                  <p className="min-w-fit grid place-content-center px-2 border-2 rounded-lg border-[#E1E3E2] rounded-tr-none rounded-br-none border-r-0 font-base text-gray-300">
+                  <p
+                    className={`min-w-fit grid place-content-center px-2 border-2 rounded-lg ${
+                      failedChecks.includes('url') ? 'border-red-205' : 'border-[#E1E3E2]'
+                    } rounded-tr-none rounded-br-none border-r-0 font-base text-gray-300`}
+                  >
                     Type link
                   </p>
 
@@ -215,7 +257,9 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
                     onChange={(e) => {
                       setLink(e.target.value);
                     }}
-                    className="border-[#E1E3E2] w-full h-[50px] rounded-md border-[2px] rounded-tl-none rounded-bl-none text-[14px] font-semibold"
+                    className={`${
+                      failedChecks.includes('url') ? 'border-red-205' : 'border-[#E1E3E2]'
+                    } w-full h-[50px] rounded-md border-[2px] rounded-tl-none rounded-bl-none text-[14px] font-semibold`}
                     inputSize={'lg'}
                     value={link}
                   />
@@ -257,7 +301,9 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
                   placeHolder="Enter your tag and press 'ENTER'"
                   onKeyDown={handleAddTags}
                   onChange={(e) => setTagInput(e.target.value)}
-                  className="border-[#E1E3E2] w-full h-[50px]  rounded-md border-[2px] text-[12px] font-semibold"
+                  className={`${
+                    failedChecks.includes('tags') ? 'border-red-205' : 'border-[#E1E3E2]'
+                  } w-full h-[50px]  rounded-md border-[2px] text-[12px] font-semibold`}
                   inputSize={'lg'}
                   value={tagInput}
                 />
@@ -272,7 +318,9 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
                   onChange={(e) => {
                     setDescription(e.target.value);
                   }}
-                  className="border-[#E1E3E2] w-full h-[50px]  rounded-md border-[2px] text-[12px] font-semibold"
+                  className={`${
+                    failedChecks.includes('description') ? 'border-red-205' : 'border-[#E1E3E2]'
+                  } w-full h-[50px]  rounded-md border-[2px] text-[12px] font-semibold`}
                   inputSize={'lg'}
                   value={description}
                 />
@@ -297,10 +345,10 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
                     </div>
                   </div>
                 ))}
-              {media.length < 10 && (
+              {media && media.length < 10 && (
                 <label
                   htmlFor="mediaUpload"
-                  className="rounded-lg px-2 py-1 bg-green-50 text-[12px] flex justify-center items-center gap-1 cursor-pointer aspect-square"
+                  className="rounded-lg px-2 py-1 bg-green-50 text-[12px] flex justify-center items-center"
                 >
                   <Add className="text-white-100" size={42} />
                   <input
@@ -318,21 +366,6 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
               Note: you can only add 10 images. Sizes 1080 X 566{' '}
             </p>
 
-            <section className="flex flex-col gap-2">
-              {failedChecks.length > 0 && (
-                <p className="mt-4 text-base font-medium text-start text-red-300">
-                  {' '}
-                  Kindly fill the following fields:{' '}
-                </p>
-              )}
-              {failedChecks.map((item) => (
-                <span className="text-sm text-start text-red-300 pl-5" key={item}>
-                  {' '}
-                  {item}{' '}
-                </span>
-              ))}
-            </section>
-
             {/* buttons */}
             <div className="my-10 flex gap-4 justify-end items-center">
               <Button intent={'secondary'} className="rounded-lg min-w-[100px]">
@@ -349,7 +382,7 @@ const ProjectSection: React.FC<ProjectSectionProps> = ({ onCloseModal, userId, o
           </form>
         </div>
       </div>
-    </>
+    </section>
   );
 };
 
