dbce931ddc619d19814753292e7da6d20a11214a
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-10-18 20:33:50 +0100

    Merge branch 'dev' into ZA-016/fix/microsoft-edge/login-page

--- pages/marketplace/wishlist.tsx
+++ pages/marketplace/wishlist.tsx
@@ -27,8 +27,8 @@ const WishlistPage = () => {
           name="image"
           content="screen image representing items on your wishlist"
         />
-  
-          {/* <!-- Twitter Meta Tags --> */}
+
+        {/* <!-- Twitter Meta Tags --> */}
         <meta key="twitter:title" name="twitter:title" content="Zuri Portfolio - Wishlist" />
         <meta
           key="twitter:description"
@@ -37,15 +37,11 @@ const WishlistPage = () => {
         />
         <meta key="twitter:image" name="twitter:image" content="https://staging.zuri.team/marketplace/wishlist" />
 
-        
         {/* <!-- Facebook Meta Tags --> */}
         <meta property="og:url" content="https://staging.zuri.team/marketplace/wishlist" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Zuri Portfolio - Wishlist" />
-        <meta
-          property="og:description"
-          content="Explore and manage your wishlist on Zuri Portfolio."
-        />
+        <meta property="og:description" content="Explore and manage your wishlist on Zuri Portfolio." />
       </Head>
       <Wishlist />
     </>
