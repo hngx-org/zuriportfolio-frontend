3ae663ac12ffca8958154fb6cf344c674e5b35eb
segunojo1 <olusegunkd2017@gmail.com>
2023-10-18 19:54:16 +0100

    sanctioned and deleted products

--- pages/super-admin/product-listing/index.tsx
+++ pages/super-admin/product-listing/index.tsx
@@ -3,9 +3,11 @@ import ProductsListingNavbar from '@modules/super-admin/components/product-listi
 import ProductListingTable from '@modules/super-admin/components/product-listing/ProductListingTable';
 import { useGetProd } from '../../../http/super-admin1';
 import { withAdminAuth } from '../../../helpers/withAuth';
+import { useState } from 'react';
 
 const ProductListing = () => {
-  const { data, isLoading } = useGetProd();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, isLoading } = useGetProd(currentPage, '');
   return (
     <div>
       <SuperAdminNavbar />
--- pages/super-admin/product-listing/sanctioned-products/index.tsx
+++ pages/super-admin/product-listing/sanctioned-products/index.tsx
@@ -1,4 +1,4 @@
-import { ArrowDown, More } from 'iconsax-react';
+import { ArrowDown, More, SearchNormal1 } from 'iconsax-react';
 
 import SuperAdminNavbar from '@modules/super-admin/components/navigations/SuperAdminNavbar';
 import SearchProduct from '@modules/super-admin/components/product-listing/searchProduct';
@@ -10,35 +10,33 @@ import { DeletedProducts } from '../../../../@types';
 import { LoadingTable } from '@modules/super-admin/components/product-listing/ProductListingTable';
 import { formatDate } from '@modules/super-admin/components/product-listing/product-details';
 import { withAdminAuth } from '../../../../helpers/withAuth';
+import { Input } from '@ui/Input';
 
 const SanctionedProducts = () => {
   const [searchVal, setSearchVal] = useState('');
-  const { data, isLoading } = useGetProd();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, isLoading } = useGetProd(currentPage, searchVal, 'sanctioned');
   const [sanctionedProducts, setSanctionedProducts] = useState<DeletedProducts[]>(data);
 
-  const sanctionedProd = data?.data?.filter((item: any) => item?.product_status === 'Sanctioned');
+  const sanctionedProd = data?.data;
 
   const [filteredProducts, setFilteredProducts] = useState(sanctionedProd);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10; // Number of items to display per page
-
-  // Calculate the range of products to display
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const visibleProducts = filteredProducts?.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(filteredProducts?.length / itemsPerPage);
 
   const handlePageChange = (newPage: number) => {
     window.scroll(0, 10);
     setCurrentPage(newPage);
   };
 
+  // useEffect(() => {
+  //   // setFilteredProducts(sanctionedProd);
+  //   setSearchVal(searchVal)
+  //   handleSearch(searchVal)
+  //   /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  // }, [searchVal, sanctionedProd]);
+
   useEffect(() => {
     setFilteredProducts(sanctionedProd);
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [sanctionedProducts]);
-  useEffect(() => {}, [filteredProducts]);
+  }, [sanctionedProd]);
 
   const handleSearch = (searchText: string) => {
     const filteredProduct: any = data?.data?.filter(
@@ -63,14 +61,27 @@ const SanctionedProducts = () => {
             <p className="text-custom-color2 text-sm">List of all sanctioned products and their details</p>
           </div>
           <div>
-            <SearchProduct handleSearchChange={handleSearch} />
+            <div className="w-[400px]">
+              <Input
+                onChange={(e) => {
+                  handleSearch(e.target.value);
+                  console.log(searchVal);
+                }}
+                leftIcon={<SearchNormal1 />}
+                type="text"
+                intent={'default'}
+                disabled={false}
+                className="md:min-w-[350px] w-[100%]"
+                placeHolder="search"
+              />
+            </div>
           </div>
         </div>
         {isLoading ? (
           <LoadingTable />
         ) : (
           <div className="mb-4">
-            {visibleProducts?.length > 0 ? (
+            {data?.data?.length > 0 ? (
               <>
                 <table className="w-full md:table-fixed">
                   <thead>
@@ -92,7 +103,7 @@ const SanctionedProducts = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {visibleProducts?.map((product: any) => (
+                    {filteredProducts.map((product: any) => (
                       <tr
                         className="border-t  border-custom-color1 cursor-pointer transition delay-100 hover:bg-white-200 py-4"
                         key={product?.product_id}
@@ -141,11 +152,11 @@ const SanctionedProducts = () => {
                     ))}
                   </tbody>
                 </table>
-                {/* <SuperAdminPagination
+                <SuperAdminPagination
                   currentPage={currentPage}
-                  totalPages={totalPages}
-                  onPageChange={handlePageChange}
-                /> */}
+                  totalPages={data.total_pages}
+                  setCurrentPage={setCurrentPage}
+                />
               </>
             ) : (
               <p className="text-red-100 my-10 w-fit mx-auto">Nothing to show</p>
