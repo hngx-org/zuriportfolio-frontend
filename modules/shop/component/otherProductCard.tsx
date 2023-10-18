371a2c80151f069d1dcd8b137c83bc622ba717db
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-10-18 20:33:50 +0100

    Merge branch 'dev' into ZA-016/fix/microsoft-edge/login-page

--- modules/shop/component/otherProductCard.tsx
+++ modules/shop/component/otherProductCard.tsx
@@ -15,20 +15,19 @@ const OtherProductCard: React.FC<ProductCardProps> = ({ product }) => {
 
   return (
     <div className="p-[0.66rem] md:p-4 shadow border h-auto sm:h-[22.75rem] md:h-auto rounded-md bg-[#ffffff]">
-      
-        <Link href={`/shop/product?id=${product.id}`} passHref>
+      <Link href={`/shop/product?id=${product.id}`} passHref>
         <div className="relative w-full h-[7.5rem] sm:h-[70%] md:h-[13.0625rem]">
           <Image
             src={product.image[0].url}
             alt={product.name}
             fill
-            style={{objectFit:"cover"}}
+            style={{ objectFit: 'cover' }}
             sizes="(max-width: 780px) 100vw, (max-width: 1024px) 50vw, 700px"
             priority
             className="rounded-md "
           />
         </div>
-        </Link>{' '}
+      </Link>{' '}
       <div className="flex flex-col gap-[0.33rem] flex-grow">
         <div>
           <h3 className="mt-2 text-sm text-[#052011] font-normal capitalize truncate md:tracking-[0.00088rem]">
