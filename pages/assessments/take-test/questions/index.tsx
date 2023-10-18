55635e288520a8427c0a85197156a1cce75f39fd
nwocha <77916165+chiomanwocha@users.noreply.github.com>
2023-10-18 01:00:31 +0100

    Merge branch 'dev' into fix(super-admin1)/hot-fixes2

--- pages/assessments/take-test/questions/index.tsx
+++ pages/assessments/take-test/questions/index.tsx
@@ -4,14 +4,13 @@ import Head from 'next/head';
 import MainLayout from '../../../../components/Layout/MainLayout';
 import { TimerStart } from 'iconsax-react';
 import { AssessmentBanner } from '@modules/assessment/component/banner';
-import { DATA } from '@modules/assessment/mock-data';
+// import { DATA } from '@modules/assessment/mock-data';
 import Link from 'next/link';
 import Button from '@ui/Button';
 import { CountdownTimer } from '@modules/assessment/CountdownTimer';
 import OutOfTime from '@modules/assessment/modals/OutOfTime';
 import { useRouter } from 'next/router';
-import { fetchUserTakenAssessment, getAssessmentDetails, submitAssessment } from '../../../../http/userTakenAssessment';
-import { any } from 'zod';
+import { fetchUserTakenAssessment, getAssessmentDetails } from '../../../../http/userTakenAssessment';
 
 type AssessmentDetails = {
   id?: string;
@@ -33,11 +32,29 @@ const Questions: React.FC = () => {
   const tokenRef = useRef<string | null>(null);
   const { data } = router.query;
   const { id } = router.query;
-
+  const [minute, setMinute] = React.useState<number | null>(null);
+  const [second, setSecond] = React.useState<number | null>(null);
+  const [duration, setDuration] = React.useState<number | null>(null);
   useEffect(() => {
     tokenRef.current = localStorage.getItem('zpt');
     handleGetStarted();
-  }, []);
+    const setTimeFunction =() => {
+      if (typeof window !== 'undefined' && window.localStorage) {
+        const minuteString = localStorage.getItem('minute');
+        const secondString = localStorage.getItem('second');
+        const minuteInt = minuteString !== null ? parseInt(minuteString, 10) : duration;
+        const secondInt = secondString !== null ? parseInt(secondString, 10) : 0;
+        setMinute(minuteInt);
+        setSecond(secondInt);
+        console.log(minuteString, secondString);
+        console.log('donezo', minute, second,duration);
+      } else {
+        throw new Error('localStorage is not available on the server-side.');
+      }
+    }
+    setTimeFunction();
+  }, [duration, minute, second]);
+ 
 
   const handleGetStarted = async () => {
     const token = tokenRef.current;
@@ -47,28 +64,11 @@ const Questions: React.FC = () => {
 
       setResult(assessmentsData);
       setStoredAssessment(questionData.questions);
+      setDuration(assessmentsData?.duration_minutes)
       console.log('2', assessmentsData);
       console.log('3', questionData.questions);
     } catch (error) {
       console.log('catch error', error);
-    }
-  };
-
-  const handleUserAnswerClick = async (question_id: number, user_answer_id: number, answer_text: string) => {
-    const token = tokenRef.current;
-    console.log(question_id, user_answer_id, answer_text, result?.assessment_id);
-    try {
-      if (token && result?.assessment_id) {
-        await submitAssessment({
-          assessment_id: result.assessment_id,
-          question_id,
-          user_answer_id,
-          answer_text,
-          token,
-        });
-      }
-    } catch (error) {
-      console.error('Error submitting assessment:', error);
     }
   };
 
@@ -107,7 +107,11 @@ const Questions: React.FC = () => {
         <div className="w-full md:max-w-xl max-w-xs mt-8 mb-16 mx-auto font-manropeL flex flex-col items-stretch justify-between gap-y-8">
           <div className="w-full lg:max-w-lg md:max-w-full sm:mx-w-xs rounded-lg flex  items-center justify-between  py-4 px-8 bg-brand-green-primary">
             <span className="text-white-100 text-2xl font-bold">
-              <CountdownTimer action={() => setIsTimeOut(true)} minutes={result?.duration_minutes ?? 29} seconds={0} />
+              {minute !== null && second !== null ? (
+                <CountdownTimer action={() => setIsTimeOut(true)} minutes={minute} seconds={second} />
+              ) : (
+                '--:--'
+              )}
             </span>
             <span>
               <TimerStart color="#fff" />
@@ -125,14 +129,8 @@ const Questions: React.FC = () => {
                   <div className="mt-4 flex gap-4 flex-col">
                     {question.options.map((option: any, index: number) => (
                       <div key={index} className="flex items-center gap-5 ">
-                        <input
-                          type="radio"
-                          id={`${option[index]}`}
-                          name={question.question_id}
-                          value={option[index]}
-                          onClick={() => handleUserAnswerClick(question.question_id, question.answer_id, option)}
-                        />
-                        <label className="text-xs text-gray-700 " htmlFor={`${option}`}>
+                        <input type="radio" id={`${option[index]}`} name={question.question_id} value={option[index]} />
+                        <label className="text-xs text-gray-700 " htmlFor={`${option.answer}`}>
                           {option}
                         </label>
                       </div>
@@ -141,7 +139,7 @@ const Questions: React.FC = () => {
                 </li>
               ))}
             </ul>
-            <Link href={`/assessments/overview?data=${result?.assessment_id}`}>
+            <Link href="/assessments/overview">
               <Button
                 intent={'primary'}
                 size={'md'}
