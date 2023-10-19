1d3313ca846d6c52d90816acdae71029a3e8bc03
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-10-19 15:44:54 -0700

    minor fixes

--- pages/super-admin/vendor-management/banned-vendors.tsx
+++ pages/super-admin/vendor-management/banned-vendors.tsx
@@ -83,9 +83,7 @@ const Index = () => {
                     {/* <p className="hidden lg:block">Action</p> */}
                   </div>
 
-                  <div>
-                    {data.data?.map((data: any) => <VendorLists key={data?.id} data={data} vendor_status="banned" />)}
-                  </div>
+                  <div>{data.data?.map((data: any) => <VendorLists key={data?.id} data={data} />)}</div>
                   <SuperAdminPagination
                     currentPage={currentPage}
                     totalPages={data?.total_pages}
