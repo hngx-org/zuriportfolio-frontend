9fd95e3ed120e9a1e2fc298622702570759ad1c9
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-10-20 20:40:48 +0100

    almost done

--- components/Loader/loader2.tsx
+++ components/Loader/loader2.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import loder from '../../public/assets/loader/loader.svg';
+import loader from '../../public/assets/loader/loader2.svg';
 
 const Loader2: React.FC = () => {
   const centerStyles: React.CSSProperties = {
@@ -11,27 +11,19 @@ const Loader2: React.FC = () => {
   };
 
   const textStyles: React.CSSProperties = {
-    fontSize: '18px',
+    fontSize: '24px',
     color: '#009254',
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: '10px',
-    fontFamily: 'Manrope',
+    fontFamily: '{Manrope}',
   };
 
   return (
     <div style={centerStyles}>
-      <p style={textStyles}>Please Wait. Your page is Loading.</p>
-
-      <br />
-      <br />
       <div className="relative flex justify-center items-center">
         <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-green-500"></div>
-        <img
-          src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg"
-          alt="Loader"
-          className="rounded-full h-28 w-28"
-        />
+        <Image src={loader} alt="Loader" className="rounded-full h-28 w-28" />
       </div>
     </div>
   );
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,23 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import Loader2 from '../../components/Loader/loader2';
-import Loader1 from '../../components/Loader/Loader1';
-import Loader3 from '../../components/Loader/Loader3';
-import Loader4 from '../../components/Loader/Loader4';
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate loading for 3 seconds
     setTimeout(() => {
       setIsLoading(false);
-    }, 10000);
+    }, 20000);
   }, []);
 
-  //return <div className="App">{isLoading ? <Loader1 /> : <p>Your content here</p>}</div>;
-  //return <div className="App">{isLoading ? <Loader2 /> : <p>Your content here</p>}</div>;
-  return <div className="App">{isLoading ? <Loader3 /> : <p>Your content here</p>}</div>;
-  //return <div className="App">{isLoading ? <Loader4/> : <p>Your content here</p>}</div>;
+  return <div className="App">{isLoading ? <Loader2 /> : <p>Your content here</p>}</div>;
 };
 
 export default App;
+
+// For API fetch Call response
+
+{
+  /*
+
+import React, { useState, useEffect } from 'react';
+import Loader2 from '../../components/Loader/loader2';
+
+const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Make an API request here
+    fetch('your_api_endpoint_here')
+      .then(response => response.json())
+      .then(data => {
+        // Once the API call is successful, set isLoading to false
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error('API request error:', error);
+      });
+  }, []);
+
+  return <div className="App">{isLoading ? <Loader2 /> : <p>Your content here</p>}</div>;
+};
+
+export default App;
+*/
+}
