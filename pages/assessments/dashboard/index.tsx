883932a900f130b3ac7303dd8a8951d14c248302
segunojo1 <olusegunkd2017@gmail.com>
2023-10-21 14:01:50 +0100

    Merge branch 'dev' of https://github.com/hngx-org/zuriportfolio-frontend into super-admin/seo

--- pages/assessments/dashboard/index.tsx
+++ pages/assessments/dashboard/index.tsx
@@ -10,6 +10,7 @@ import timer from '../../../public/assets/dashboard/timer.svg';
 import medal from '../../../public/assets/dashboard/medal-star.svg';
 import { useRouter } from 'next/router';
 import { withUserAuth } from '../../../helpers/withAuth';
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 import Loader from '@ui/Loader';
 
 type AssessmentDetails = {
@@ -22,12 +23,21 @@ type AssessmentDetails = {
   status: string;
   start_date: Date;
   end_date: Date;
+  taken?:boolean
 };
 
 const Dashboard = () => {
   const [result, setResult] = React.useState<AssessmentDetails[]>([]);
+  const [resultTap, setResultTap] = React.useState<AssessmentDetails[]>([]);
   const [isLoading, setIsLoading] = React.useState<boolean>(true);
   const router = useRouter();
+  const queryClient = useQueryClient();
+  const token = localStorage.getItem('zpt');
+  // Fetch all the user's assessments
+  const { data: assessments } = useQuery(['assessments'], () => getAllAssessments(token as string));
+
+  const newAssessments = assessments?.assessments;
+  console.log('new', newAssessments);
 
   React.useEffect(() => {
     const token = localStorage.getItem('zpt');
@@ -125,7 +135,7 @@ const Dashboard = () => {
                 </div>
               </div>
             </div>
-            {result.length > 0 ? (
+            {newAssessments?.length > 0 ? (
               result.map((item, index) => (
                 <div
                   key={index}
@@ -145,7 +155,7 @@ const Dashboard = () => {
                   </div>
                   <div className="mt-[1rem]">
                     <p className="text-[#2E3130] text-[.5rem] md:text-[.625rem] lg:text-[.85rem] xl:text-[1.rem] leading-1">
-                      Unlock your potential and level up your skills – take the assessment now!
+                      Unlock your potential and level up your skills - take the assessment now!
                     </p>
 
                     <div className="flex items-center gap-[1rem] xl:gap-[2rem] my-[1rem] px-3 ml-3">
@@ -188,7 +198,7 @@ const Dashboard = () => {
                   </div>
                   <Link href={`/assessments/take-test/intro?data=${item.skill_id}`}>
                     <Button className="mt-[1.5rem] lg:mt-[1.8rem] xl:mt-[2.9rem] mx-auto text-[.6rem] md:text-[.75rem] lg:text-[.95rem] xl:text-[1.125rem] py-[.8rem] lg:py-[1rem] xl:py-[1.3rem] h-0 rounded-md">
-                      Take Assessment
+                      {item.taken===true?"Retake assessment":"Take Assessment"}
                     </Button>
                   </Link>
                 </div>
