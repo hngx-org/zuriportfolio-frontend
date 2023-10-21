b50b28fea19afd3de7ce22771b3ee6e2515875bb
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-10-21 22:10:35 +0100

    Merge pull request #1384 from hngx-org/CRE-12/Feat/Create-assessment
    Cre 12/feat/create assessment

--- modules/assessment/component/Assessmentresponses.tsx
+++ modules/assessment/component/Assessmentresponses.tsx
@@ -128,84 +128,90 @@ function Assessmentresponses(props: PropsAss<any>) {
           </div>
         </div>
       )}
-      {assessments?.map((child: any) => {
-        return (
-          <div
-            className="w-full flex items-center justify-between border-[1px] rounded p-4 md:p-6  border-brand-disabled2"
-            key={child?.id}
-          >
-            <div>
-              <Link
-                href={`/super-admin/assessment/assessment?assessment_id=${child.id}`}
-                className="text-custom-color11 text-base md:text-[18px] font-manropeB font-bold mb-[4px]"
-              >
-                {child?.title}
-              </Link>
-              <div className="my-[6px] md:my-[10px] text-white-650">
-                {' '}
-                Created {formatDateToPattern(child?.createdAt)}
-              </div>
-              <div className="text-white-650">Modified {formatDateToPattern(child?.updatedAt)}</div>
-            </div>
-            <div className="flex gap-2 md:gap-8">
-              <div className="flex flex-col items-center cursor-pointer">
-                <Image src={editmessage} height="24" width="24" alt="edit message" />
+      {assessments?.length > 0 ? (
+        assessments?.map((child: any) => {
+          return (
+            <div
+              className="w-full flex items-center justify-between border-[1px] rounded p-4 md:p-6  border-brand-disabled2"
+              key={child?.id}
+            >
+              <div>
                 <Link
-                  href={`/super-admin/assessment/preview-edit/?assessmentId=${child.id}`}
-                  className="text-xs md:text-base pt-[6px]"
+                  href={`/super-admin/assessment/assessment?assessment_id=${child.id}`}
+                  className="text-custom-color11 text-base md:text-[18px] font-manropeB font-bold mb-[4px]"
                 >
-                  Edit
+                  {child?.title}
                 </Link>
+                <div className="my-[6px] md:my-[10px] text-white-650">
+                  {' '}
+                  Created {formatDateToPattern(child?.createdAt)}
+                </div>
+                <div className="text-white-650">Modified {formatDateToPattern(child?.updatedAt)}</div>
               </div>
-              <div
-                className="cursor-pointer flex flex-col items-center justify-center gap-y-1"
-                onClick={() => {
-                  delitem(child?.id);
-                }}
-              >
-                <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M21 6.47998C17.67 6.14998 14.32 5.97998 10.98 5.97998C9 5.97998 7.02 6.07998 5.04 6.27998L3 6.47998"
-                    stroke="#464646"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M8.5 5.47L8.72 4.16C8.88 3.21 9 2.5 10.69 2.5H13.31C15 2.5 15.13 3.25 15.28 4.17L15.5 5.47"
-                    stroke="#464646"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M18.8499 9.64001L18.1999 19.71C18.0899 21.28 17.9999 22.5 15.2099 22.5H8.7899C5.9999 22.5 5.9099 21.28 5.7999 19.71L5.1499 9.64001"
-                    stroke="#464646"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M10.3301 17H13.6601"
-                    stroke="#464646"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M9.5 13H14.5"
-                    stroke="#464646"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-                Delete
+              <div className="flex gap-2 md:gap-8">
+                <div className="flex flex-col items-center cursor-pointer">
+                  <Image src={editmessage} height="24" width="24" alt="edit message" />
+                  <Link
+                    href={`/super-admin/assessment/preview-edit/?assessmentId=${child.id}`}
+                    className="text-xs md:text-base pt-[6px]"
+                  >
+                    Edit
+                  </Link>
+                </div>
+                <div
+                  className="cursor-pointer flex flex-col items-center justify-center gap-y-1"
+                  onClick={() => {
+                    delitem(child?.id);
+                  }}
+                >
+                  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M21 6.47998C17.67 6.14998 14.32 5.97998 10.98 5.97998C9 5.97998 7.02 6.07998 5.04 6.27998L3 6.47998"
+                      stroke="#464646"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M8.5 5.47L8.72 4.16C8.88 3.21 9 2.5 10.69 2.5H13.31C15 2.5 15.13 3.25 15.28 4.17L15.5 5.47"
+                      stroke="#464646"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M18.8499 9.64001L18.1999 19.71C18.0899 21.28 17.9999 22.5 15.2099 22.5H8.7899C5.9999 22.5 5.9099 21.28 5.7999 19.71L5.1499 9.64001"
+                      stroke="#464646"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M10.3301 17H13.6601"
+                      stroke="#464646"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M9.5 13H14.5"
+                      stroke="#464646"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  Delete
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <div className="w-full flex items-center justify-between border-[1px] rounded p-4 md:p-6  border-brand-disabled2">
+          No assessments created yet! Today is a good day too
+        </div>
+      )}
     </div>
   );
 }
