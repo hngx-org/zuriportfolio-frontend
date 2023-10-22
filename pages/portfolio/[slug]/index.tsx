1021e2f3a2e74ec62dc1e2f4a552f818f7c10907
MIRACLE OKEKE <mokeke185@gmail.com>
2023-10-22 22:30:45 +0100

    Merge branch 'dev' into HAR-1/Feat/Zuri-Shop-Landing-Page

--- pages/portfolio/[slug]/index.tsx
+++ pages/portfolio/[slug]/index.tsx
@@ -7,41 +7,69 @@ import Cover from '@modules/portfolio/component/landing/cover-avatar';
 import Image from 'next/image';
 import { CoverDiv } from '@modules/portfolio/component/landing/avatars';
 import { useRouter } from 'next/router';
-import Loader from '@modules/portfolio/component/landing/Loader';
-import withAuth from '../../../helpers/withAuth';
+import SkeletonLoader from '@modules/portfolio/component/landing/SkeletonLoader';
 
 const View = () => {
   const router = useRouter();
   const urlSlug = router.query.slug;
 
-  // Auth to get userid
-  // const { auth } = useAuth();
-
-  // const urlSlug = Array.isArray(router?.query?.slug) ? router?.query?.slug[0] : router?.query?.slug;
-
-  // const params = useParams();
-  // useEffect(() => {
-  //   console.log("Slug",urlSlug);
-
-  //   // console.log(params);
-  //   // wait for router to be ready
-  //   if (!router.isReady) return;
-  //   if (!auth?.user?.slug) {
-  //     return
-  //   }
-  //   // if user is logged in and user id is same as id in url, redirect to dashboard
-  //   if (auth?.user?.slug === urlSlug) {
-  //     router.push(`/portfolio`);
-  //   } else {
-  //     // if user is not logged in and id is not in url, fetch info with the id from url
-  //     if (urlSlug) {
-  //       getUser();
-  //     }
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
-  // auth?.user?.slug, urlSlug, router
+  useEffect(() => {
+    if (urlSlug) {
+      const getUser = async () => {
+        try {
+          setIsLoading(true);
+          const response = await fetch(`https://hng6-r5y3.onrender.com/api/v1/portfolio/${urlSlug}`);
+          const data = await response.json();
+          if (!response.ok) throw new Error(data.error);
+          setUserData({
+            firstName: data?.data?.user?.firstName,
+            lastName: data?.data?.user?.lastName,
+            avatarImage: data?.data?.user?.profilePic,
+            city: data?.data?.portfolio?.city,
+            country: data?.data?.portfolio?.country,
+            tracks: data?.data?.tracks,
+            coverImage: data?.data?.user?.profileCoverPhoto,
+          });
+          const {
+            about,
+            projects,
+            workExperience,
+            education,
+            skills,
+            contact,
+            interestArray,
+            awards,
+            language,
+            reference,
+            certificates,
+            shop,
+            custom,
+          } = data.data;
+          setUserSections([
+            { title: 'About', id: 'about', data: about },
+            { title: 'Project', id: 'projects', data: projects },
+            { title: 'Work Experience', id: 'workExperience', data: workExperience },
+            { title: 'Education', id: 'education', data: education },
+            { title: 'Skills', id: 'skills', data: skills },
+            { title: 'Interests', id: 'interests', data: interestArray },
+            { title: 'Awards', id: 'awards', data: awards },
+            { title: 'Certificate', id: 'certificate', data: certificates },
+            { title: 'Language', id: 'language', data: language },
+            { title: 'Reference', id: 'reference', data: reference },
+            { title: 'Shop', id: 'shop', data: shop },
+            { title: 'Contact', id: 'contact', data: contact },
+            { title: 'Custom', id: 'custom', data: custom },
+          ]);
+          setIsLoading(false);
+        } catch (error: any) {
+          setIsLoading(false);
+          setError({ state: true, error: error.message });
+          setIsLoading(false);
+        }
+      };
+      getUser();
+    }
+  }, [urlSlug]);
 
   useEffect(() => {
     if (urlSlug) {
@@ -128,56 +156,49 @@ const View = () => {
   return (
     <PortfolioCtxProvider>
       <MainLayout showTopbar showDashboardSidebar={false} activePage="portfolio" showFooter>
-        {isLoading ? (
-          <>
-            <Loader />
-          </>
-        ) : error.state ? (
-          <div className="flex justify-center items-center min-h-[50vh]">
-            <p className="text-red-200 text-2xl font-semibold text-center">
-              Something went wrong, please try again later
-            </p>
-          </div>
-        ) : (
-          <div className="mx-auto w-[min(90vw,1240px)] relative font-manropeB pb-20 min-h-[50vh]">
-            <div className="relative w-full flex-col justify-center items-center shadow-[0_0px_6px_1px_rgba(0,0,0,0.14)] rounded-b-lg -mt-5 mb-10">
-              {cover}
-              <Cover userData={userData} />
-              <div className="flex justify-between items-baseline px-5 md:px-10 pb-5 pt-16">
-                <div className="flex justify-between items-center ">
-                  <div>
-                    <h1 className="font-semibold text-lg md:text-[23px] text-gray-700">
-                      {firstName === 'undefined' || !firstName ? '' : firstName}{' '}
-                      {lastName === 'undefined' || !lastName ? '' : lastName}
-                    </h1>
-                    <div className="flex items-center space-x-2">
-                      {tracks && (
-                        <p className="flex flex-col text-gray-500 font-semibold text-[15px]">{tracks?.track}</p>
-                      )}
+        <div className="mx-auto w-[min(90vw,1240px)] relative font-manropeB pb-20 min-h-[50vh]">
+          {isLoading ? (
+            <>
+              {' '}
+              <SkeletonLoader pulse={true} error={false} message={''} />
+            </>
+          ) : error.state ? (
+            <>
+              {' '}
+              <SkeletonLoader pulse={false} error={true} message={'Something went wrong, please try again later'} />
+            </>
+          ) : (
+            <>
+              <div className="relative w-full flex-col justify-center items-center shadow-[0_0px_6px_1px_rgba(0,0,0,0.14)] rounded-b-lg -mt-5 mb-10">
+                {cover}
+                <Cover userData={userData} />
+                <div className="flex justify-between items-baseline px-5 md:px-10 pb-5 pt-16">
+                  <div className="flex justify-between items-center ">
+                    <div>
+                      <h1 className="font-semibold text-lg md:text-[23px] text-gray-700">
+                        {firstName === 'undefined' || !firstName ? '' : firstName}{' '}
+                        {lastName === 'undefined' || !lastName ? '' : lastName}
+                      </h1>
+                      <div className="flex items-center space-x-2">
+                        {tracks && (
+                          <p className="flex flex-col text-gray-500 font-semibold text-[15px]">{tracks?.track}</p>
+                        )}
+                      </div>
+                      <p className="text-gray-500 text-[14px] md:text-base font-semibold">
+                        {city ? city : ``}
+                        {`${city && country ? ',' : ''}`} {country ? country : ''}
+                      </p>
                     </div>
-                    <p className="text-gray-500 text-[14px] md:text-base font-semibold">
-                      {city ? city : ``}
-                      {`${city && country ? ',' : ''}`} {country ? country : ''}
-                    </p>
                   </div>
                 </div>
               </div>
-            </div>
-            <ExternalView userSections={userSections} />
-          </div>
-        )}
+              <ExternalView userSections={userSections} />
+            </>
+          )}
+        </div>
       </MainLayout>
     </PortfolioCtxProvider>
   );
 };
 
 export default View;
-
-export async function getServerSideProps(context: any) {
-  const { slug } = context.query;
-  return {
-    props: {
-      userslug: slug,
-    },
-  };
-}
