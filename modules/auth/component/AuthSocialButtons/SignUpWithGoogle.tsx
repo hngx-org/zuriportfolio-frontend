3fb505c64b46b7251d60bcc9f5a3fad2c3f01c8a
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2023-11-27 11:44:52 +0100

    Merge branch 'dev' into Great/Fix-3-Question-logic-fix

--- modules/auth/component/AuthSocialButtons/SignUpWithGoogle.tsx
+++ modules/auth/component/AuthSocialButtons/SignUpWithGoogle.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import SignUpWithSocialsButton from './SignUpWithSocialsButton';
 import googleLogo from '../../../../public/assets/Google.png';
+import LogoCarousel from '@modules/home/carousel/logos/logosCarousel';
+
 
 const SignUpWithGoogle = () => {
+
+  const [isloading, setIsLoading] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      window.location.href="https://zuri-auth.up.railway.app/api/auth/api/auth/google";
+    }, 5000);
+  };
+
+//href="https://zuri-auth.up.railway.app/api/auth/api/auth/google"
   return (
     // the google logo has white space around it, so i am reducing the margin on the right so all the buttons look similar
     <SignUpWithSocialsButton
-      href="https://staging.zuri.team/api/auth/api/auth/google"
+    //  href="https://zuri-auth.up.railway.app/api/auth/api/auth"
+        onClick={handleLinkClick}
+        isLoading={isloading}
+       //onClick={handleGoogleSignIn}
       leftIcon={<Image src={googleLogo} alt="Google Logo" className="w-5 h-5" />}
     >
       Continue with Google
