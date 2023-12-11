c2e29a328813c841713a148d7a2e9c92531d9efd
IbiminaFG <gogoibimina@gmail.com>
2023-12-11 12:01:04 +0100

    Fixed the google sign for the login page

--- modules/auth/component/AuthSocialButtons/SignUpWithGoogle.tsx
+++ modules/auth/component/AuthSocialButtons/SignUpWithGoogle.tsx
@@ -4,27 +4,25 @@ import SignUpWithSocialsButton from './SignUpWithSocialsButton';
 import googleLogo from '../../../../public/assets/Google.png';
 import LogoCarousel from '@modules/home/carousel/logos/logosCarousel';
 
-
 const SignUpWithGoogle = () => {
-
   const [isloading, setIsLoading] = useState(false);
 
   const handleLinkClick = () => {
     setIsLoading(true);
     setTimeout(() => {
-      window.location.href="https://zuri-auth.up.railway.app/api/auth/api/auth/google";
+      window.location.href = 'https://zuri-auth.up.railway.app/api/auth/api/auth/google';
     }, 5000);
   };
 
-//href="https://zuri-auth.up.railway.app/api/auth/api/auth/google"
+  //href="https://zuri-auth.up.railway.app/api/auth/api/auth/google"
   return (
     // the google logo has white space around it, so i am reducing the margin on the right so all the buttons look similar
     <SignUpWithSocialsButton
-    //  href="https://zuri-auth.up.railway.app/api/auth/api/auth"
-        onClick={handleLinkClick}
-        isLoading={isloading}
-       //onClick={handleGoogleSignIn}
-      leftIcon={<Image src={googleLogo} alt="Google Logo" className="w-5 h-5" />}
+      //  href="https://zuri-auth.up.railway.app/api/auth/api/auth"
+      onClick={handleLinkClick}
+      isLoading={isloading}
+      //onClick={handleGoogleSignIn}
+      leftIcon={<Image src={googleLogo} alt="Google Logo" className="" />}
     >
       Continue with Google
     </SignUpWithSocialsButton>
