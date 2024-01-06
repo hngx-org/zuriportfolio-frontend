cbcb2aa9999bf431e256f12cbfe7ec500fbb8dc4
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-01-06 17:27:46 +0100

    Merge pull request #1538 from hngx-org/Phoenix
    Phoenix

---
+++ modules/portfolio/component/Breadcrumbs.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+function Breadcrumbs() {
+  const router = useRouter();
+  const routeArr = router.route.slice(1).split('/');
+
+  const breadcrumbs = routeArr.map((el, i, arr) => (
+    <React.Fragment key={i}>
+      <span className="mx-2">{'>'}</span>
+      <Link
+        href={`/${arr.slice(0, i).join('/')}/${el}`}
+        className={`${i + 1 === arr.length ? ' text-green-600 font-manropeB' : null}`}
+      >
+        {el.split('-').join(' ')}
+      </Link>
+    </React.Fragment>
+  ));
+
+  return (
+    <div className="font-manropeB font-medium text-xl text-[#5B5F5E] capitalize">
+      <div>
+        <Link href="/">Home</Link>
+        {breadcrumbs}
+      </div>
+    </div>
+  );
+}
+
+export default Breadcrumbs;
